0996e898c670ed748b6f95a096f735479d29eb21
Fabian Bader <fabian.bader@glueckkanja-gab.com>
2023-12-05 09:06:31 +0100

    Initial version

---
+++ src/public/Test-MtConditionalAccessWhatIf.ps1
@@ -0,0 +1,75 @@
+
+
+Function Test-MtConditionalAccessWhatIf {
+    [CmdletBinding(DefaultParameterSetName = 'ApplicationBasedCA')]
+    [OutputType([object])]
+    param (
+        # The UserId to test the Conditional Acccess policie with
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0, Mandatory)]
+        [string]$UserId,
+
+        # The applications that should be tested Default: All
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'ApplicationBasedCA')]
+        [string[]]$IncludeApplications = "All",
+
+        # The user action that should be tested. Default: registerOrJoinDevices
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'UserActionBasedCA')]
+        [ValidateSet("registerOrJoinDevices", "registerSecurityInformation")]
+        [string[]]$UserAction = "registerOrJoinDevices",
+
+        # The device platform that should be tested. Default: windows
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("Android", "iOS", "windows", "windowsPhone", "macOS", "linux")]
+        [string]$DevicePlatform = "windows",
+
+        # The client app used by the user. Default: all
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("all", "browser", "mobileAppsAndDesktopClients", "exchangeActiveSync", "easSupported", "other")]
+        [string]$ClientAppType = "all",
+
+        # The sign in risk level of the user sign-in. Default: None
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("None", "Low", "Medium", "High")]
+        [string]$SignInRiskLevel = "None",
+
+        # The user risk level of the user signing in. Default: None
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("None", "Low", "Medium", "High")]
+        $UserRiskLevel = "None"
+    )
+
+    process {
+        # Definition of conditional access
+        if ($UserAction) {
+            $CAContext = @{
+                "@odata.type" = "#microsoft.graph.whatIfUserActionContext"
+                "userAction"  = $UserAction
+            }
+        } else {
+            $CAContext = @{
+                "@odata.type"         = "#microsoft.graph.whatifapplicationContext"
+                "includeApplications" = @(
+                    $IncludeApplications
+                )
+            }
+        }
+
+        $ConditionalAccessWhatIfDefinition = @{
+            "conditionalAccessWhatIfSubject"    = @{
+                "@odata.type" = "#microsoft.graph.userSubject"
+                "userId"      = "$userId"
+            }
+            "conditionalAccessContext"          = $CAContext
+            "conditionalAccessWhatIfConditions" = @{
+                "signInRiskLevel" = $SignInRiskLevel
+                "clientAppType"   = $ClientAppType
+                "devicePlatform"  = $DevicePlatform
+            }
+        }
+
+        Write-Verbose ( $ConditionalAccessWhatIfDefinition | ConvertTo-Json -Depth 99 -Compress )
+
+        $ConditionalAccessWhatIfResult = Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/beta/identity/conditionalAccess/evaluate" -Body ( $ConditionalAccessWhatIfDefinition | ConvertTo-Json -Depth 99 -Compress ) | Select-Object -ExpandProperty value | Where-Object { $_.policyApplies -eq $true }
+        return $ConditionalAccessWhatIfResult
+    }
+}
