62cc06cdf669d34689955f0a0b0d4c83f9e2ba73
Fabian Bader <fabian.bader@toolsection.info>
2024-01-04 20:51:45 +0100

    Add ID1005 - Emergency account check

--- src/Maester.psd1
+++ src/Maester.psd1
@@ -69,9 +69,9 @@
     NestedModules        = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport    = 'Get-MtConditionalAccessPolicies',
-    'Test-MtAppManagementPolicyEnabled', 'Test-MtCaAllAppsExists',
-    'Test-MtCaDeviceComplianceExists', 'Test-MtConditionalAccessWhatIf'
+    FunctionsToExport    = @('Get-MtConditionalAccessPolicies',
+        'Test-MtAppManagementPolicyEnabled', 'Test-MtCaAllAppsExists',
+        'Test-MtCaDeviceComplianceExists', 'Test-MtConditionalAccessWhatIf', 'Test-MtCaEmergencyAccessExists')
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport      = @()
---
+++ src/public/Test-MtCaEmergencyAccessExists.ps1
@@ -0,0 +1,37 @@
+<#
+ .Synopsis
+  Checks if the tenant has at least one emergency account or account group excluded from all conditional access policies
+
+ .Description
+  It is recommended to have at least one emergency account or account group excluded from all conditional access policies.
+  This allows for emergency access to the tenant in case of a misconfiguration or other issues.
+
+  Learn more:
+  https://learn.microsoft.com/en-us/entra/identity/role-based-access-control/security-emergency-access
+
+ .Example
+  Test-MtCaEmergencyAccessExists
+#>
+
+Function Test-MtCaEmergencyAccessExists {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param ()
+
+    $policies = Get-MtConditionalAccessPolicies | Select-Object -ExpandProperty value | Where-Object { $_.state -eq "enabled" }
+
+    Set-StrictMode -Off
+
+    $result = $false
+    $PolicyCount = $policies | Measure-Object | Select-Object -ExpandProperty Count
+    $ExcludedUsers = $policies.conditions.users.excludeUsers | Group-Object -NoElement | Sort-Object -Property Count -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Count
+    $ExcludedGroups = $policies.conditions.users.excludeGroups | Group-Object -NoElement | Sort-Object -Property Count -Descending | Select-Object -First 1 | Select-Object -ExpandProperty Count
+    # If the number of enabled policies is not the same as the number of excluded users or groups, there is no emergency access
+    if ($PolicyCount -eq $ExcludedUsers -or $PolicyCount -eq $ExcludedGroups) {
+        $result = $true
+    }
+
+    Set-StrictMode -Version Latest
+
+    return $result
+}
