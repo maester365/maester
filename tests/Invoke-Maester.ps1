487cfb37cb80e0ebd42d0dc49ea635dec5eb36de
Fabian Bader <fabian.bader@toolsection.info>
2024-01-20 20:23:08 +0100

    Remove legacy code from Invoke-Maester.ps1

--- tests/Invoke-Maester.ps1
+++ tests/Invoke-Maester.ps1
@@ -5,10 +5,10 @@ $motd = @"
 
 .___  ___.      ___       _______     _______.___________. _______ .______         ____    ____  ___      __
 |   \/   |     /   \     |   ____|   /       |           ||   ____||   _  \        \   \  /   / / _ \    /_ |
-|  \  /  |    /  ^  \    |  |__     |   (----`---|  |----`|  |__   |  |_)  |        \   \/   / | | | |    | |
+|  \  /  |    /  ^  \    |  |__     |   (--------|  |----``|  |__   |  |_)  |        \   \/   / | | | |    | |
 |  |\/|  |   /  /_\  \   |   __|     \   \       |  |     |   __|  |      /          \      /  | | | |    | |
 |  |  |  |  /  _____  \  |  |____.----)   |      |  |     |  |____ |  |\  \----.      \    /   | |_| |  __| |
-|__|  |__| /__/     \__\ |_______|_______/       |__|     |_______|| _| `._____|       \__/     \___/  (__)_|
+|__|  |__| /__/     \__\ |_______|_______/       |__|     |_______|| _| ``._____|       \__/     \___/  (__)_|
 
 
 "@
@@ -20,9 +20,13 @@ $RequiredScopes = @(
     'Policy.ReadWrite.ConditionalAccess'
 )
 $CurrentScopes = Get-MgContext | Select-Object -ExpandProperty Scopes
-$RequiredScopesOkay = [bool][string]::IsNullOrWhiteSpace( $( Compare-Object -ReferenceObject $CurrentScopes -DifferenceObject $RequiredScopes | Where-Object { $_.SideIndicator -eq '=>' } | Select-Object -ExpandProperty InputObject ) )
+try {
+    $RequiredScopesOkay = [bool][string]::IsNullOrWhiteSpace( $( Compare-Object -ReferenceObject $CurrentScopes -DifferenceObject $RequiredScopes | Where-Object { $_.SideIndicator -eq '=>' } | Select-Object -ExpandProperty InputObject ) )
+} catch {
+    $RequiredScopesOkay = $false
+}
 
-if ( -not $RequiredScopesOkay ) {
+if ( $RequiredScopesOkay ) {
     Connect-MgGraph -UseDeviceAuthentication -Scope $RequiredScopes
 }
 
@@ -30,32 +34,6 @@ $PSDefaultParameterValues = @{
     'Invoke-MgGraphRequest:Verbose' = $false
 }
 
-#region Define variables to match your environment
-
-$BreakGlassUserIds = @("a5032592-a8c0-4b9c-9b62-0d137cfede11")
-
-# Create a hash table with Azure AD users to test, you can change the number of users to test
-$TestNumberOfUsers = 1
-$TestAzureADUser = New-Object -TypeName 'System.Collections.ArrayList'
-$AADUsers = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users?`$top=$($TestNumberOfUsers+10)" | Select-Object -ExpandProperty value | Select-Object id, userPrincipalName
-# Filter out the break glass users and limit it to $TestNumberOfUsers
-$AADUsers = $AADUsers | Where-Object { $_.id -notcontains $BreakGlassUserIds } | Select-Object -First $TestNumberOfUsers
-foreach ($User in $AADUsers) {
-    $TempHashTable = @{
-        userId            = $User.id
-        userPrincipalName = $User.userPrincipalName
-    }
-    $TestAzureADUser.Add($TempHashTable) | Out-Null
-}
-#endregion
-
-$configRunContainer = @(
-    # Add global variables to the container
-    New-PesterContainer -Path "*.Tests.ps1" -Data @{
-        AzureADUser       = $TestAzureADUser
-        BreakGlassUserIds = $BreakGlassUserIds
-    }
-)
 
 $PesterConfiguration = New-PesterConfiguration -Hashtable @{
     Filter     = @{
@@ -66,8 +44,7 @@ $PesterConfiguration = New-PesterConfiguration -Hashtable @{
         Enabled = $true
     }
     Run        = @{
-        Exit      = $true
-        Container = $configRunContainer
+        Exit = $true
     }
     Should     = @{
         ErrorAction = 'Continue'
