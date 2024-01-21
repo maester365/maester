6890e23d391a5109ac7d2a691d0b4d23a1f9986b
Fabian Bader <fabian.bader@toolsection.info>
2024-01-21 16:03:16 +0100

    Refactor Test-MtCaEmergencyAccessExists.ps1 to check for emergency/break glass accounts or groups in conditional access policies

--- tests/Identity/Test-ConditionalAccessBaseline.Tests.ps1
+++ tests/Identity/Test-ConditionalAccessBaseline.Tests.ps1
@@ -9,7 +9,7 @@ Describe "Conditional Access Baseline Policies" -Tag "CA", "Security", "All" {
     It "ID1004: At least one Conditional Access policy is configured with All Apps and All Users. See https://maester.dev/t/ID1004" {
         Test-MtCaAllAppsExists | Should -Be $true -Because "There is no policy scoped to All Apps and All Users"
     }
-    It "ID1005: All Conditional Access policies are configured to exclude at least one emergency account or group. See https://maester.dev/t/ID1005" {
+    It "ID1005: All Conditional Access policies are configured to exclude at least one emergency/break glass account or group. See https://maester.dev/t/ID1005" {
         Test-MtCaEmergencyAccessExists | Should -Be $true -Because "There is no emergency access account or group present in all enabled policies"
     }
     It "ID1006: At least one Conditional Access policy is configured to require MFA for admins. See https://maester.dev/t/ID1006" {
