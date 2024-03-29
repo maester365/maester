ced270a9d1aae951f55df17004d1b78900f2ff9d
Fabian Bader <fabian.bader@toolsection.info>
2024-03-29 19:25:49 +0100

    Add detected emergency access account or group to message

--- powershell/public/Test-MtCaEmergencyAccessExists.ps1
+++ powershell/public/Test-MtCaEmergencyAccessExists.ps1
@@ -39,6 +39,8 @@ Function Test-MtCaEmergencyAccessExists {
 It is recommended to have at least one emergency/break glass account or account group excluded from all conditional access policies.
 This allows for emergency access to the tenant in case of a misconfiguration or other issues.
 
+Detected emergency access account or group: $CheckId
+
 See [Manage emergency access accounts in Microsoft Entra ID - Microsoft Learn](https://learn.microsoft.com/entra/identity/role-based-access-control/security-emergency-access)"
         $testResult = "These conditional access policies don't have the emergency access account or group excluded:`n`n"
         # Check if the emergency access account or group is excluded from all policies and write verbose output
