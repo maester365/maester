09551080cae1ebede3e3e39ba669ec1a8447b754
Merill Fernando <merill@merill.net>
2024-03-30 10:54:06 +1100

    Added support for converting graph objects to markdown

--- powershell/public/Add-MtTestResultDetail.ps1
+++ powershell/public/Add-MtTestResultDetail.ps1
@@ -13,40 +13,72 @@
     This example adds detailed information about a test with a brief description and the result of the test.
 
     ```powershell
-            # Markdown should start from the beginning of the line with no leading spaces
-            $testDescription = "
-It is recommended to have at least one emergency/break glass account or account group excluded from all conditional access policies.
-This allows for emergency access to the tenant in case of a misconfiguration or other issues.
+        $policiesWithoutEmergency = $policies | Where-Object { $CheckId -notin $_.conditions.users.excludeUsers -and $CheckId -notin $_.conditions.users.excludeGroups }
 
-See [Manage emergency access accounts in Microsoft Entra ID - Microsoft Learn](https://learn.microsoft.com/entra/identity/role-based-access-control/security-emergency-access)"
-        $testResult = "These conditional access policies don't have the emergency access account or group excluded:`n`n"
-        # Check if the emergency access account or group is excluded from all policies and write verbose output
-        $policies | Where-Object { $CheckId -notin $_.conditions.users.excludeUsers -and $CheckId -notin $_.conditions.users.excludeGroups } | Select-Object -ExpandProperty displayName | Sort-Object | ForEach-Object {
-            $testResult += "  - [$_](https://entra.microsoft.com/#view/Microsoft_AAD_ConditionalAccess/PolicyBlade/policyId/$($_.id)?%23view/Microsoft_AAD_ConditionalAccess/ConditionalAccessBlade/~/Policies?=)`n"
-            Write-Verbose "Conditional Access policy $_ does not exclude emergency access account or group"
-        }
-        Add-MtTestResultDetail -Description $testDescription -Result $testResult
+        Add-MtTestResultDetail -GraphObjects $policiesWithoutEmergency -GraphObjectType ConditionalAccess
     ```
+
+    This example shows how to use the Add-MtTestResultDetail function to add rich markdown content to the test results with deep links to the admin portal.
 #>
 
 Function Add-MtTestResultDetail {
     [CmdletBinding()]
     param(
         # Brief description of what this test is checking.
-        [Parameter(Mandatory = $true)]
+        # Markdown is supported.
+        [Parameter(Mandatory = $false)]
         [string] $Description,
 
         # Detailed information of the test result to provide additional context to the user.
         # This can be a summary of the items that caused the test to fail (e.g. list of user names, conditional access policies, etc.).
-        [Parameter(Mandatory = $true)]
-        [string] $Result
+        # Markdown is supported.
+        # If the test result contains a placeholder %TestResult%, it will be replaced with the values from the $GraphResult
+        [Parameter(Mandatory = $false)]
+        [string] $Result,
+
+        # Collection of Graph objects to display in the test results report.
+        # This will be inserted into the contents of Result parameter if the result contains a placeholder %TestResult%.
+        [Object[]] $GraphObjects,
+
+        # The type of graph object, this will be used to show the right deeplink to the test results report.
+        [ValidateSet('ConditionalAccess', 'Users', 'Groups')]
+        [string] $GraphObjectType
     )
 
     $testName = $____Pester.CurrentTest.Name # Get the test name from the Pester context.
+
+    if ([string]::IsNullOrEmpty($Description)) {
+        # Check if a markdown file exists for the cmdlet and parse the content
+        $cmdletPath = $MyInvocation.PSCommandPath
+        $markdownPath = $cmdletPath -replace '.ps1', '.md'
+        if (Test-Path $markdownPath) {
+            # Read the content and split it into description and result with "<!--- Results --->" as the separator
+            $content = Get-Content $markdownPath -Raw
+            $splitContent = $content -split "<!--- Results --->"
+            $mdDescription = $splitContent[0]
+            $mdResult = $splitContent[1]
+
+            $Description = $mdDescription
+            $Result = $mdResult
+        }
+    }
+
+    if($GraphObjects -and $GraphObjectType) {
+        $graphResultMarkdown = Get-GraphObjectMarkdown -GraphObjects $GraphObjects -GraphObjectType $GraphObjectType
+        $Result = $Result -replace "%TestResult%", $graphResultMarkdown
+    }
 
     $testInfo = @{
         TestDescription = $Description
         TestResult      = $Result
     }
-    $MtTestResultDetail[$testName] = $testInfo
+
+    Write-Verbose "Adding test result detail for $testName"
+    Write-Verbose "Description: $Description"
+    Write-Verbose "Result: $Result"
+
+    if ($null -ne $MtTestResultDetail -and ![string]::IsNullOrEmpty($testName)) {
+        # Only set if we are running in the context of Maester
+        $MtTestResultDetail[$testName] = $testInfo
+    }
 }
