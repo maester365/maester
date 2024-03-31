5420e9c518574097d6e2a2f99885ddf0535801f8
Fabian Bader <fabian.bader@toolsection.info>
2024-03-31 16:13:25 +0200

    Add Test-MtCaMfaForAllUsers.md and update Test-MtCaMfaForAllUsers.ps1

--- powershell/public/Test-MtCaMfaForAllUsers.ps1
+++ powershell/public/Test-MtCaMfaForAllUsers.ps1
@@ -13,7 +13,7 @@
 #>
 
 Function Test-MtCaMfaForAllUsers {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification='AllUsers is a well known term for conditional access policies.')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'AllUsers is a well known term for conditional access policies.')]
     [CmdletBinding()]
     [OutputType([bool])]
     param ()
@@ -21,12 +21,7 @@ Function Test-MtCaMfaForAllUsers {
     $policies = Get-MtConditionalAccessPolicy | Where-Object { $_.state -eq "enabled" }
     # Remove policies that require password change, as they are related to user risk and not MFA on signin
     $policies = $policies | Where-Object { $_.grantcontrols.builtincontrols -notcontains 'passwordChange' }
-
-    $testDescription = "
-This test checks if the tenant has at least one conditional access policy requiring MFA for all users.
-
-See [Require MFA for all users - Microsoft Learn](https://learn.microsoft.com/entra/identity/conditional-access/howto-conditional-access-policy-all-users-mfa)"
-    $testResult = "The following conditional access policies require multi-factor authentication for all users:`n`n"
+    $policiesResult = New-Object System.Collections.ArrayList
 
     $result = $false
     foreach ($policy in $policies) {
@@ -37,17 +32,19 @@ See [Require MFA for all users - Microsoft Learn](https://learn.microsoft.com/en
         ) {
             $result = $true
             $currentresult = $true
-            $testResult += "  - [$($policy.displayname)](https://entra.microsoft.com/#view/Microsoft_AAD_ConditionalAccess/PolicyBlade/policyId/$($($policy.id))?%23view/Microsoft_AAD_ConditionalAccess/ConditionalAccessBlade/~/Policies?=)`n"
+            $policiesResult.Add($policy) | Out-Null
         } else {
             $currentresult = $false
         }
         Write-Verbose "$($policy.displayName) - $currentresult"
     }
 
-    if ( $result -eq $false ) {
-        $testResult = "No conditional access policy requires multi-factor authentication for all users"
+    if ( $result ) {
+        $testResult = "The following conditional access policies require multi-factor authentication for all users:`n`n%TestResult%"
+    } else {
+        $testResult = "No conditional access policy requires multi-factor authentication for all users."
     }
-    Add-MtTestResultDetail -Description $testDescription -Result $testResult
+    Add-MtTestResultDetail -Result $testResult -GraphObjects $policiesResult -GraphObjectType ConditionalAccess
 
     return $result
 }
