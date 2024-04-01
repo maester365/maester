fecc75f6bbadfcb00497684f7ebc9ea5780756a8
Fabian Bader <fabian.bader@toolsection.info>
2024-04-01 21:44:39 +0200

    Update user counts and add tests for emergency access users

--- tests/Maester/Entra/Test-ConditionalAccessWhatIf.Tests.ps1
+++ tests/Maester/Entra/Test-ConditionalAccessWhatIf.Tests.ps1
@@ -1,17 +1,34 @@
 BeforeDiscovery {
     $EntraIDPlan = Get-MtLicenseInformation -Product "EntraID"
-    $RegularUsers = Get-MtUser -Count 2 -UserType "Member"
-    $AdminUsers = Get-MtUser -Count 1 -UserType "Admin"
+    $RegularUsers = Get-MtUser -Count 5 -UserType "Member"
+    $AdminUsers = Get-MtUser -Count 5 -UserType "Admin"
+    $EmergencyAccessUsers = Get-MtUser -Count 5 -UserType "EmergencyAccess"
+    # Remove emergency access users from regular users
+    $RegularUsers = $RegularUsers | Where-Object { $_.id -notin $EmergencyAccessUsers.id }
+    # Remove emergency access users from admin users
+    $AdminUsers = $AdminUsers | Where-Object { $_.id -notin $EmergencyAccessUsers.id }
     Write-Verbose "EntraIDPlan: $EntraIDPlan"
     Write-Verbose "RegularUsers: $($RegularUsers.id)"
     Write-Verbose "AdminUsers: $($AdminUsers.id)"
 }
 
 
-Describe "Conditional Access WhatIf" -Tag "CA", "CAWhatIf", "Security", "All" -Skip:( $EntraIDPlan -eq "Free" ) -ForEach @( $RegularUsers ) {
+Describe "Conditional Access WhatIf" -Tag "CA", "CAWhatIf", "Security", "All" -Skip:( $EntraIDPlan -eq "Free" ) {
 
-    It "MT.1023: User should be blocked from using legacy authentication (<userPrincipalName>)" {
-        Test-MtCaWIFBlockLegacyAuthentication -UserId $id | Should -Be $true
+    Context "Regular users" -ForEach @( $RegularUsers ) {
+
+        It "MT.1023: User should be blocked from using legacy authentication (<userPrincipalName>)" {
+            Test-MtCaWIFBlockLegacyAuthentication -UserId $id | Should -Be $true
+        }
+
+    }
+
+    Context "Emergency access users" -ForEach @( $EmergencyAccessUsers ) {
+
+        It "MT.1023: Emergency access users should not be blocked (<userPrincipalName>)" {
+            Test-MtConditionalAccessWhatIf -UserId $id -IncludeApplications "00000002-0000-0ff1-ce00-000000000000" -ClientAppType exchangeActiveSync | Should -BeNullOrEmpty
+        }
+
     }
 
 }
