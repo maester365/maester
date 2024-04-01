23351065624ae5a3b85cbf36c7238d84ef2fb7e7
Fabian Bader <fabian.bader@toolsection.info>
2024-04-01 20:52:54 +0200

    Add detailed test description and update Entra Recommendations test

--- tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
+++ tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
@@ -6,10 +6,13 @@ BeforeDiscovery {
 
 Describe "Entra Recommendations" -Tag "Entra", "Security", "All" -ForEach $EntraRecommendations {
     It "Entra Recommendation: <displayName>" {
+        #region Add detailed test description
         $ActionSteps = $actionSteps | Sort-Object -Property 'stepNumber' | Select-Object -ExpandProperty text -EA SilentlyContinue
         $ActionSteps = $ActionSteps -join "`n`n"
         $ResultMarkdown = $insights + "`n`nRemediation actions:`n`n" + $ActionSteps
-        Add-MtTestResultDetail -TestName "Entra Recommendation: $($displayName)" -Description $benefits -Result $ResultMarkdown
+        Add-MtTestResultDetail -Description $benefits -Result $ResultMarkdown
+        #endregion
+        # Actual test
         $status | Should -Be "completedBySystem" -Because $benefits
     }
 }
