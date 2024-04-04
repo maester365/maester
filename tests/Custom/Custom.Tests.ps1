814a6c6721793bdbb853fe4ccfc1d45205c265b6
Merill Fernando <merill@merill.net>
2024-04-04 16:07:32 +1100

    Added custom tests

---
+++ tests/Custom/Custom.Tests.ps1
@@ -0,0 +1,24 @@
+﻿Describe "ContosoConfig" -Tag "Privilege" {
+    It "Check approved Global Admins" {
+
+        # Approved list of Global Admins
+        $approvedGlobalAdmins = @("john@contoso.com", "emergency@contoso.com")
+
+        # Get all Global Admins
+        $roleId = (Get-MgDirectoryRole -Filter "DisplayName eq 'Global Administrator'").Id
+        $globalAdmins = Get-MgDirectoryRoleMember -DirectoryRoleId $roleId
+        $globalAdmins = $globalAdmins.AdditionalProperties.userPrincipalName
+
+        # Check if the Global Admins are approved
+        $globalAdmins | Should -BeIn $approvedGlobalAdmins
+    }
+}
+
+
+
+
+
+
+
+
+
