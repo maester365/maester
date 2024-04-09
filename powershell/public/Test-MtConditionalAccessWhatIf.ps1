41fd15161381ae652aebc01fb216decd7d268e2e
merill <merill@users.noreply.github.com>
2024-04-08 23:50:53 -0700

    Added support for ipAddress, country and fixed clientAppType null

--- powershell/public/Test-MtConditionalAccessWhatIf.ps1
+++ powershell/public/Test-MtConditionalAccessWhatIf.ps1
@@ -11,7 +11,14 @@
     https://learn.microsoft.com/entra/identity/conditional-access/what-if-tool
 
 .EXAMPLE
-    TODO
+    Test-MtConditionalAccessWhatIf -UserId 7a6da1c3-616a-416b-a820-cbe4fa8e225e -IncludeApplications "00000002-0000-0ff1-ce00-000000000000" -ClientAppType exchangeActiveSync
+
+    This example tests the Conditional Access policies for a user signing into Exchange Online using a legacy Mail client that relies on basic authentication.
+
+    Test-MtConditionalAccessWhatIf -UserId 7a6da1c3-616a-416b-a820-cbe4fa8e225e -UserAction registerOrJoinDevices
+
+    This example tests the Conditional Access policies for a user signing into Exchange Online using a legacy Mail client that relies on basic authentication.
+
 #>
 function Test-MtConditionalAccessWhatIf {
     [CmdletBinding(DefaultParameterSetName = 'ApplicationBasedCA')]
@@ -22,36 +29,45 @@ function Test-MtConditionalAccessWhatIf {
         [ValidateScript({ $_ -match '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$' })]
         [string]$UserId,
 
-        # The applications that should be tested Default: All
+        # The applications that should be tested
         # Must be a valid application ID (GUID)
         [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = "ApplicationBasedCA", Mandatory)]
         [ValidateScript({ $_ -match '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$' })]
         [string[]]$IncludeApplications,
 
-        # The user action that should be tested. Default: registerOrJoinDevices
+        # The user action that should be tested.
         [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = "UserActionBasedCA")]
         [ValidateSet("registerOrJoinDevices", "registerSecurityInformation")]
-        [string[]]$UserAction = "registerOrJoinDevices",
+        [string[]]$UserAction,
 
-        # The device platform that should be tested. Default: windows
+        # The device platform that should be tested.
         [Parameter(ValueFromPipelineByPropertyName = $true)]
-        [ValidateSet("Android", "iOS", "windows", "windowsPhone", "macOS", "linux")]
-        [string]$DevicePlatform = "windows",
+        [ValidateSet("all", "Android", "iOS", "windows", "windowsPhone", "macOS", "linux")]
+        [string]$DevicePlatform,
 
-        # The client app used by the user. Default: all
+        # The client app used by the user.
         [Parameter(ValueFromPipelineByPropertyName = $true)]
-        [ValidateSet("all", "browser", "mobileAppsAndDesktopClients", "exchangeActiveSync", "easSupported", "other")]
-        [string]$ClientAppType = "all",
+        [ValidateSet("browser", "mobileAppsAndDesktopClients", "exchangeActiveSync", "easSupported", "other")]
+        [string]$ClientAppType,
 
-        # The sign in risk level of the user sign-in. Default: None
-        [Parameter(ValueFromPipelineByPropertyName = $true)]
-        [ValidateSet("None", "Low", "Medium", "High")]
-        [string]$SignInRiskLevel = "None",
-
-        # The user risk level of the user signing in. Default: None
+        # The sign in risk level of the user sign-in.
         [Parameter(ValueFromPipelineByPropertyName = $true)]
         [ValidateSet("None", "Low", "Medium", "High")]
-        [string]$UserRiskLevel = "None",
+        [string]$SignInRiskLevel,
+
+        # The user risk level of the user signing in.
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("None", "Low", "Medium", "High")]
+        [string]$UserRiskLevel,
+
+        # The country of the user signing in.
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW")]
+        [string]$Country,
+
+        # The ipAddress of the user signing in.
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [string]$IpAddress,
 
         # Output all results
         [Parameter()]
@@ -61,9 +77,14 @@ function Test-MtConditionalAccessWhatIf {
     process {
         # Definition of conditional access
         if ($PSCmdlet.ParameterSetName -eq "UserActionBasedCA") {
+            if ($UserAction.Length -eq 1) {
+                $UserActionValue = $UserAction[0] # Array not supported by userAction when there is only one item.
+            } else {
+                $UserActionValue = $UserAction
+            }
             $CAContext = @{
                 "@odata.type" = "#microsoft.graph.whatIfUserActionContext"
-                "userAction"  = $UserAction
+                "userAction"  = $UserActionValue
             }
         } else {
             $CAContext = @{
@@ -80,13 +101,17 @@ function Test-MtConditionalAccessWhatIf {
                 "userId"      = $UserId
             }
             "conditionalAccessContext"          = $CAContext
-            "conditionalAccessWhatIfConditions" = @{
-                "userRiskLevel"   = $UserRiskLevel
-                "signInRiskLevel" = $SignInRiskLevel
-                "clientAppType"   = $ClientAppType
-                "devicePlatform"  = $DevicePlatform
-            }
+            "conditionalAccessWhatIfConditions" = @{}
         }
+
+        $whatIfConditions = $ConditionalAccessWhatIfDefinition.conditionalAccessWhatIfConditions
+
+        if ($UserRiskLevel) { $whatIfConditions.userRiskLevel = $UserRiskLevel }
+        if ($SignInRiskLevel) { $whatIfConditions.signInRiskLevel = $SignInRiskLevel }
+        if ($ClientAppType) { $whatIfConditions.clientAppType = $ClientAppType }
+        if ($DevicePlatform) { $whatIfConditions.devicePlatform = $DevicePlatform }
+        if ($Country) { $whatIfConditions.country = $Country }
+        if ($IpAddress) { $whatIfConditions.ipAddress = $IpAddress }
 
         Write-Verbose ( $ConditionalAccessWhatIfDefinition | ConvertTo-Json -Depth 99 -Compress )
 
@@ -102,3 +127,4 @@ function Test-MtConditionalAccessWhatIf {
         }
     }
 }
+
