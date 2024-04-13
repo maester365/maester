64762ecb2d098efd848b9331bd91028d0d46abda
Fabian Bader <fabian.bader@toolsection.info>
2024-04-13 17:02:53 +0200

    Update Invoke-MgGraphRequest to Invoke-MtGraphRequest in Test-PrivilegedAssignments.Tests.ps1

--- tests/Maester/Entra/Test-PrivilegedAssignments.Tests.ps1
+++ tests/Maester/Entra/Test-PrivilegedAssignments.Tests.ps1
@@ -1,5 +1,5 @@
 ﻿BeforeDiscovery {
-    $AvailablePlans = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/beta/organization" | Select-Object -ExpandProperty value | Select-Object -ExpandProperty assignedPlans | Where-Object service -EQ "AADPremiumService" | Select-Object -ExpandProperty servicePlanId
+    $AvailablePlans = Invoke-MtGraphRequest -RelativeUri "organization" | Select-Object -ExpandProperty assignedPlans | Where-Object service -EQ "AADPremiumService" | Select-Object -ExpandProperty servicePlanId
     if ( "eec0eb4f-6444-4f95-aba0-50c24d67f998" -in $AvailablePlans ) {
         $EntraIDPlan = "P2"
     } elseif ( "41781fb2-bc02-4b7c-bd55-b576c07bb09d)" -in $AvailablePlans ) {
