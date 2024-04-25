a0f35ccad924008ab1b963bd327cf1f5b06e2d7f
merill <merill@users.noreply.github.com>
2024-04-25 14:17:55 +1000

    Updated script to pick current version from psgallery

--- .github/workflows/minor-version-update.ps1
+++ .github/workflows/minor-version-update.ps1
@@ -10,8 +10,9 @@ if ( -not (Test-Path $ManfifestPath )) {
     Write-Error "Could not find PowerShell module manifest ($ManfifestPath)"
     throw
 } else {
-    [version]$CurrentVersion = Import-PowerShellDataFile $ManfifestPath | Select-Object -ExpandProperty ModuleVersion
-    $NewVersion = "{0}.{1}.{2}" -f $CurrentVersion.Major, $CurrentVersion.Minor, ( $CurrentVersion.Build + 1 )
+    $ver = [version](Find-Module -Name Maester).Version
+    $newBuild = $ver.Build + 1
+    $NewVersion = '{0}.{1}.{2}' -f $ver.Major, $ver.Minor, $newBuild
 
     $publicScripts = @( Get-ChildItem -Path "$ModuleRoot/public" -Recurse -Filter "*.ps1" )
     $FunctionNames = @( $publicScripts.BaseName | Sort-Object )
