5491b29ed6eea45a5c4e99b4149567b32b7aefe6
Snozz <mike@soule.biz>
2024-04-28 16:33:38 -0700

    Add 3.7

---
+++ tests/CISA/Entra/Test-MtCisaManagedDevice.Tests.ps1
@@ -0,0 +1,9 @@
+BeforeDiscovery {
+    $EntraIDPlan = Get-MtLicenseInformation -Product EntraID
+}
+
+Describe "CISA SCuBA" -Tag "MS.AAD", "MS.AAD.3.7", "CISA", "Security", "All" -Skip:( $EntraIDPlan -eq "Free" ) {
+    It "MS.AAD.3.7: Managed devices SHOULD be required for authentication." {
+        Test-MtCisaManagedDevice | Should -Be $true -Because "a policy requires managed devices."
+    }
+}
