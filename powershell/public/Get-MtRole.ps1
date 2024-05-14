caff9fff223c38a491b23db08d5d065bedde4c8b
Snozz <mike@soule.biz>
2024-05-13 17:52:57 -0700

    Added parameter help message; Changed roles from displayName to id

--- powershell/public/Get-MtRole.ps1
+++ powershell/public/Get-MtRole.ps1
@@ -4,6 +4,10 @@
 
  .Description
 
+ .Parameter CisaHighlyPrivilegedRoles
+  Filters the returned roles to only those described
+  by CISA as highly privieleged.
+
  .Example
   Get-MtRole
 #>
@@ -16,14 +20,14 @@ Function Get-MtRole {
 
   #https://github.com/cisagov/ScubaGear/blob/main/PowerShell/ScubaGear/baselines/aad.md#highly-privileged-roles
   $highlyPrivilegedRoles = @(
-    "Global Administrator",
-    "Privileged Role Administrator",
-    "User Administrator",
-    "SharePoint Administrator",
-    "Exchange Administrator",
-    "Hybrid Identity Administrator",
-    "Application Administrator",
-    "Cloud Application Administrator"
+    "62e90394-69f5-4237-9190-012177145e10",
+    "fe930be7-5e62-47db-91af-98c3a49a38b1",
+    "29232cdf-9323-42fd-ade2-1d097af3e4de",
+    "f28a1f50-f6e7-4571-818b-6a12f2af6b6c",
+    "9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3",
+    "e8611ab8-c189-46e8-94e1-60213ab1f814",
+    "158c047a-c907-4556-b7ef-446551a6b5f7",
+    "8ac3fc64-6eca-42ea-9e69-59f4c7b60eb2"
   )
 
   Write-Verbose -Message "Getting directory role definitions."
