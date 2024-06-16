e1053a9c0e8487eeb57e0fc6ceca3ea8f03bb385
Merill Fernando <merill@merill.net>
2024-06-16 12:53:40 +1000

    Added error handling when module not installed

--- powershell/public/core/Test-MtConnection.ps1
+++ powershell/public/core/Test-MtConnection.ps1
@@ -19,29 +19,38 @@ Function Test-MtConnection {
     [CmdletBinding()]
     param(
         # Checks if the current session is connected to the specified service
-        [ValidateSet("All","Azure","ExchageOnline","Graph")]
+        [ValidateSet("All", "Azure", "ExchageOnline", "Graph")]
         [Parameter(Position = 0, Mandatory = $false)]
         [string[]]$Service = "Graph"
     )
 
     $connectionState = $true
 
-    if($Service -contains "Azure" -or $Service -contains "All"){
-        $isConnected = $null -ne (Get-AzContext -ErrorAction SilentlyContinue)
-        Write-Verbose "Connected to Azure: $isConnected"
-        if(!$isConnected){ $connectionState = $false }
+    if ($Service -contains "Azure" -or $Service -contains "All") {
+        $isConnected = $false
+        try {
+            $isConnected = $null -ne (Get-AzContext -ErrorAction SilentlyContinue)
+        } catch {}
+        Write-Verbose "Azure: $isConnected"
+        if (!$isConnected) { $connectionState = $false }
     }
 
-    if($Service -contains "ExchageOnline" -or $Service -contains "All"){
-        $isConnected = $null -ne ((Get-ConnectionInformation | Where-Object {$_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected'}))
-        Write-Verbose "Connected to Exchange Online: $isConnected"
-        if(!$isConnected){ $connectionState = $false }
+    if ($Service -contains "ExchageOnline" -or $Service -contains "All") {
+        $isConnected = $false
+        try {
+            $isConnected = $null -ne ((Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' }))
+        } catch { }
+        Write-Verbose "Exchange Online: $isConnected"
+        if (!$isConnected) { $connectionState = $false }
     }
 
-    if($Service -contains "Graph" -or $Service -contains "All"){
-        $isConnected = $null -ne (Get-MgContext -ErrorAction SilentlyContinue)
-        Write-Verbose "Connected to Graph: $isConnected"
-        if(!$isConnected){ $connectionState = $false }
+    if ($Service -contains "Graph" -or $Service -contains "All") {
+        $isConnected = $false
+        try {
+            $isConnected = $null -ne (Get-MgContext -ErrorAction SilentlyContinue)
+        } catch { }
+        Write-Verbose "Graph: $isConnected"
+        if (!$isConnected) { $connectionState = $false }
     }
 
     Write-Output $connectionState
