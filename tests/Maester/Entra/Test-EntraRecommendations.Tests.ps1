8632efdf8b645d98a16f59a39b2e5f785e7c5eef
Thomas S. Schmidt <thosc@tdc.dk>
2024-06-27 15:28:36 +0200

    Enhanced MT.1024 Entra Recommendations to include impacted resources where available.

--- tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
+++ tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
@@ -1,6 +1,6 @@
 BeforeDiscovery {
     $EntraIDPlan = Get-MtLicenseInformation -Product "EntraID"
-    $EntraRecommendations = Invoke-MtGraphRequest -DisableCache -ApiVersion beta -RelativeUri 'directory/recommendations' -OutputType Hashtable
+    $EntraRecommendations = Invoke-MtGraphRequest -DisableCache -ApiVersion beta -RelativeUri 'directory/recommendations?$expand=impactedResources' -OutputType Hashtable
     Write-Verbose "Found $($EntraRecommendations.Count) Entra recommendations"
 }
 
@@ -9,7 +9,19 @@ Describe "Entra Recommendations" -Tag "Entra", "Security", "All", "Recommendatio
         #region Add detailed test description
         $ActionSteps = $actionSteps | Sort-Object -Property 'stepNumber' | Select-Object -ExpandProperty text -EA SilentlyContinue
         $ActionSteps = $ActionSteps -join "`n`n"
-        $ResultMarkdown = $insights + "`n`nRemediation actions:`n`n" + $ActionSteps
+        if ($status -ne 'completedBySystem' -and $impactedResources) {
+            $impactedResourcesList = "`n`n#### Impacted resources`n`n | Status | Name | First detected| `n"
+            $impactedResourcesList += "| --- | --- | --- |`n"
+            foreach ($resource in $impactedResources) {
+                if ($resource.status -eq 'completedBySystem') {
+                    $resourceResult = "✅ Pass"
+                } else {
+                    $resourceResult = "❌ Fail"
+                }
+                $impactedResourcesList += "| $($resourceResult) | [$($resource.displayName)]($($resource.portalUrl)) | $($resource.addedDateTime) | `n"
+            }
+        }
+        $ResultMarkdown = $insights + $impactedResourcesList + "`n`n#### Remediation actions:`n`n" + $ActionSteps
         Add-MtTestResultDetail -Description $benefits -Result $ResultMarkdown
         #endregion
         # Actual test
