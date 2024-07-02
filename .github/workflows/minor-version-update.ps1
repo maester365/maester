5187ed98bcdaf44437a02b491fd15d255fe12fbe
Merill Fernando <merill@merill.net>
2024-07-02 21:11:40 +1000

    Added preview support

--- .github/workflows/minor-version-update.ps1
+++ .github/workflows/minor-version-update.ps1
@@ -1,7 +1,10 @@
 [CmdletBinding()]
 Param(
     [Parameter()]
-    [string]$ModuleRoot = "$($env:GITHUB_WORKSPACE)/powershell"
+    [string]$ModuleRoot = "$($env:GITHUB_WORKSPACE)/powershell",
+
+    [Parameter()]
+    [switch]$preview = $false
 )
 
 $ManfifestPath = "$($ModuleRoot)/Maester.psd1"
@@ -11,15 +14,18 @@ if ( -not (Test-Path $ManfifestPath )) {
     throw
 } else {
     # Get the current version of the module from the PowerShell gallery
-    $ver = [version](Find-Module -Name Maester).Version
+    $ver = [version](Find-Module -Name Maester -AllowPrerelease:$Prerelease).Version
     $newBuild = $ver.Build + 1
     $NewVersion = '{0}.{1}.{2}' -f $ver.Major, $ver.Minor, $newBuild
 
     $publicScripts = @( Get-ChildItem -Path "$ModuleRoot/public" -Recurse -Filter "*.ps1" )
     $FunctionNames = @( $publicScripts.BaseName | Sort-Object )
 
-    Update-ModuleManifest -Path $ManfifestPath -ModuleVersion $NewVersion -FunctionsToExport $FunctionNames
+    $previewLabel = if ($preview) { '-preview' } else { '' }
+
+    Update-ModuleManifest -Path $ManfifestPath -ModuleVersion $NewVersion -FunctionsToExport $FunctionNames -Prerelease $previewLabel
 }
 
+$NewVersion += $previewLabel
 Add-Content -Path $env:GITHUB_OUTPUT -Value "newtag=$NewVersion"
 Add-Content -Path $env:GITHUB_OUTPUT -Value "tag=$NewVersion"
