5ca8a9c3f9e0cd89ca3cf08022252d723716aed2
Merill Fernando <merill@merill.net>
2024-07-06 00:05:37 +1000

    Switched to incrementing minor build for each publish

--- .github/workflows/minor-version-update.ps1
+++ .github/workflows/minor-version-update.ps1
@@ -18,8 +18,19 @@ if ( -not (Test-Path $ManfifestPath )) {
     Write-Host "Previous version: $previousVersion"
 
     $ver = [version]($previousVersion -replace '-preview')
-    $newBuild = $ver.Build + 1
-    $NewVersion = '{0}.{1}.{2}' -f $ver.Major, $ver.Minor, $newBuild
+
+    # Set new version number. If it is pre-release, increment the build number otherwise increment the minor version.
+    $major = 0 # Update this to change the major version number of Maester.
+    $minor = $ver.Minor
+
+    if ($preview) {
+        $build = $ver.Build + 1
+    } else {
+        $minor = $ver.Minor + 1
+        $build = 0 # Reset the build number when incrementing the minor version.
+    }
+
+    $NewVersion = '{0}.{1}.{2}' -f $major, $minor, $build
 
     $publicScripts = @( Get-ChildItem -Path "$ModuleRoot/public" -Recurse -Filter "*.ps1" )
     $FunctionNames = @( $publicScripts.BaseName | Sort-Object )
