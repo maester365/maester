e4049e5a47565da8f1b1155e7502a90952dac687
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2024-07-13 12:29:43 +0000

    Fix test names in Module.Tests.ps1
    BeforeAll variables are not available during discovery. Replaced with template string

--- powershell/tests/general/Module.Tests.ps1
+++ powershell/tests/general/Module.Tests.ps1
@@ -3,7 +3,7 @@
     $moduleRoot = "$PSScriptRoot/../.."
 }
 
-Describe "$module Module Tests" -Tags ('Unit', 'Acceptance') {
+Describe "<module> Module Tests" -Tags ('Unit', 'Acceptance') {
 
     Context 'Module Setup' {
         It "has the root module $module.psm1" {
@@ -15,11 +15,11 @@ Describe "$module Module Tests" -Tags ('Unit', 'Acceptance') {
             Join-Path -Path $moduleRoot -ChildPath "$module.psd1" | Should -FileContentMatch "$module.psm1"
         }
 
-        It "$module folder has functions" {
+        It '<module> folder has functions' {
             Join-Path -Path $moduleRoot -ChildPath "public/*.ps1" | Should -Exist
         }
 
-        It "$module is valid PowerShell code" {
+        It '<module> is valid PowerShell code' {
             $psFile = Get-Content -Path (Join-Path -Path $moduleRoot -ChildPath "$module.psm1") -ErrorAction Stop
             $errors = $null
             $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
