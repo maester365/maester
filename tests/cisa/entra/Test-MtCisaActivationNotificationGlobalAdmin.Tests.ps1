17cd6b1ee9846922645d6fc97daf92d755dad511
Snozz <mike@soule.biz>
2024-07-13 19:09:47 -0700

    Fix tests

--- tests/cisa/entra/Test-MtCisaActivationNotificationGlobalAdmin.Tests.ps1
+++ tests/cisa/entra/Test-MtCisaActivationNotificationGlobalAdmin.Tests.ps1
@@ -1,6 +1,6 @@
 Describe "CISA SCuBA" -Tag "MS.AAD", "MS.AAD.7.8", "CISA", "Security", "All" {
     It "MS.AAD.7.8: User activation of the Global Administrator role SHALL trigger an alert." {
-        $result = Test-MtCisaActivationNotification
+        $result = Test-MtCisaActivationNotification -GlobalAdminOnly
 
         if ($null -ne $result) {
             $result | Should -Be $true -Because "notifications are set for activation of the Global Admin role."
--- tests/cisa/entra/Test-MtCisaAuthenticatorContext.Tests.ps1
+++ tests/cisa/entra/Test-MtCisaAuthenticatorContext.Tests.ps1
@@ -1,13 +1,9 @@
-BeforeDiscovery {
-    $EntraIDPlan = Get-MtLicenseInformation -Product EntraID
-
-    $result = Get-MtAuthenticationMethodPolicyConfig
-
-    $authenticator = $result | Where-Object { $_.id -eq "MicrosoftAuthenticator" }
-}
-
-Describe "CISA SCuBA" -Tag "MS.AAD", "MS.AAD.3.3", "CISA", "Security", "All" -Skip:( ($EntraIDPlan -eq "Free") -or (Test-MtCisaPhishResistant) -or $authenticator.state -eq "disabled") {
+Describe "CISA SCuBA" -Tag "MS.AAD", "MS.AAD.3.3", "CISA", "Security", "All" {
     It "MS.AAD.3.3: If phishing-resistant MFA has not been enforced and Microsoft Authenticator is enabled, it SHALL be configured to show login context information." {
-        Test-MtCisaAuthenticatorContext | Should -Be $true -Because "Microsoft Authenticator is configured to show login context information."
+        $result = Test-MtCisaAuthenticatorContext
+
+        if ($null -ne $result) {
+            $result | Should -Be $true -Because "Microsoft Authenticator is configured to show login context information."
+        }
     }
 }
