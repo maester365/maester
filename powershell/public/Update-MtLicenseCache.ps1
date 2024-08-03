c9981e80f021a736239071cb92a07a08dfb15a88
Snozz <mike@soule.biz>
2024-08-03 14:58:29 -0700

    Fixes

--- powershell/public/Update-MtLicenseCache.ps1
+++ powershell/public/Update-MtLicenseCache.ps1
@@ -13,10 +13,21 @@
 #>
 function Update-MtLicenseCache {
     [OutputType([System.Void])]
-    [CmdletBinding()]
-    param ()
+    [CmdletBinding(
+        SupportsShouldProcess,
+        ConfirmImpact = "Low"
+    )]
+
+    param (
+        [Switch]$Force,
+        [String]$FileName="maesterLicenses.csv"
+    )
 
     process {
+        if ($Force -and -not $Confirm){
+            $ConfirmPreference = "None"
+        }
+
         try{
             Write-Verbose "Attempting License Cache Update"
             $csv = Invoke-RestMethod -Uri "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv"
@@ -26,16 +37,16 @@ function Update-MtLicenseCache {
             exit
         }
         Write-Verbose "Getting hash of downloaded content"
-        $hashNew = Get-FileHash -InputStream ([IO.MemoryStream]::new([text.encoding]::UTF8.GetBytes($skus2))) -Algorithm SHA256
+        $hashNew = Get-FileHash -InputStream ([IO.MemoryStream]::new([text.encoding]::UTF8.GetBytes($csv))) -Algorithm SHA256
 
-        if(Test-Path -Path $env:TEMP\maesterLicenses.csv){
+        if(Test-Path -Path $env:TEMP\$FileName){
             Write-Verbose "Cache exists"
-            $skus = Get-Content $env:TEMP\maesterLicenses.csv
+            $skus = Get-Content $env:TEMP\$FileName
             Write-Verbose "Getting cache hash"
             $hashOld = Get-FileHash -InputStream ([IO.MemoryStream]::new([text.encoding]::UTF8.GetBytes($skus))) -Algorithm SHA256
-            if($hashNew.Hash -ne $hashOld.Hash){
+            if($hashNew.Hash -ne $hashOld.Hash -and $PSCmdlet.ShouldProcess($FileName)){
                 Write-Verbose "Cache does not match download, overwriting"
-                $csv|Out-File $env:TEMP\maesterLicenses.csv
+                $csv|Out-File $env:TEMP\$FileName -Confirm:$false
                 $skus = $csv|ConvertFrom-Csv
             }else{
                 Write-Verbose "Cache matches"
@@ -43,7 +54,9 @@ function Update-MtLicenseCache {
             }
         }else{
             Write-Verbose "Cache does not exist, setting cache"
-            $csv|Out-File $env:TEMP\maesterLicenses.csv
+            if($PSCmdlet.ShouldProcess($FileName)){
+                $csv|Out-File $env:TEMP\$FileName -Confirm:$false
+            }
             $skus = $csv|ConvertFrom-Csv
         }
 
