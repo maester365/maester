ee5352deb2ff9c523eda435b05183ad9a847780e
Snozz <mike@soule.biz>
2024-08-06 18:43:25 -0700

    alternate DLP test

---
+++ powershell/public/cisa/exchange/Test-MtCisaDlpAlternate.md
@@ -0,0 +1,14 @@
+The selected DLP solution SHOULD offer services comparable to the native DLP solution offered by Microsoft.
+
+Rationale: Any alternative DLP solution should be able to detect sensitive information in Exchange Online and block access to unauthorized entities.
+
+> This test will always skip by default.
+
+#### Related links
+
+* [Purview admin center - Data loss prevention policies](https://purview.microsoft.com/datalossprevention/policies)
+* [CISA 8 Data Loss Prevention Solutions - MS.EXO.8.3](https://github.com/cisagov/ScubaGear/blob/main/PowerShell/ScubaGear/baselines/exo.md#msexo83v1)
+* [CISA ScubaGear Rego Reference](https://github.com/cisagov/ScubaGear/blob/main/PowerShell/ScubaGear/Rego/EXOConfig.rego#L453)
+
+<!--- Results --->
+%TestResult%
---
+++ powershell/public/cisa/exchange/Test-MtCisaDlpAlternate.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+    This will always return $null
+
+.DESCRIPTION
+    The selected DLP solution SHOULD offer services comparable to the native DLP solution offered by Microsoft.
+
+.EXAMPLE
+    Test-MtCisaDlpAlternate
+
+    Always will return $null
+
+.LINK
+    https://maester.dev/docs/commands/Test-MtCisaDlpAlternate
+#>
+function Test-MtCisaDlpAlternate {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param()
+
+    #Add License Check
+    if(!(Test-MtConnection ExchangeOnline)){
+        Add-MtTestResultDetail -SkippedBecause NotConnectedExchange
+        return $null
+    }elseif(!(Test-MtConnection SecurityCompliance)){
+        Add-MtTestResultDetail -SkippedBecause NotConnectedSecurityCompliance
+        return $null
+    }else{
+        return $null
+    }
+}
