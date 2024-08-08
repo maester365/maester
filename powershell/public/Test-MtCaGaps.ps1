45a365ca004fab3f90ff763d0d0072049a03f65d
Robbe Van den Daele Operator <robbe.van.den.daele.operator@thecollective.eu>
2024-08-08 23:41:44 +0200

    feat(synopsis):add basic description

---
+++ powershell/public/Test-MtCaGaps.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+    Check for gaps in conditional access policies
+
+.DESCRIPTION
+    This function checks if all objects found in policy exclusions are found in policy inclusions.
+
+.EXAMPLE
+    <TO DO>
+
+.LINK
+    <TO DO>
+#>
