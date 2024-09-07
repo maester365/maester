822fc60fc2144b4e0dfe2ca998a7682213703b1a
NZLostboy <ben@l33t.co.nz>
2024-09-07 14:33:48 +1200

    Fixed MtCisaCalenarSharing removal

---
+++ powershell/public/cisa/exchange/Test-MtCisaCalendarSharing.ps1
@@ -0,0 +1,57 @@
+﻿<#
+.SYNOPSIS
+    Checks state of sharing policies
+
+.DESCRIPTION
+    Calendar details SHALL NOT be shared with all domains.
+
+.EXAMPLE
+    Test-MtCisaCalendarSharing
+
+    Returns true if no sharing policies allow uncontrolled calendar sharing.
+
+.LINK
+    https://maester.dev/docs/commands/Test-MtCisaCalendarSharing
+#>
+function Test-MtCisaCalendarSharing {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param()
+
+    if(!(Test-MtConnection ExchangeOnline)){
+        Add-MtTestResultDetail -SkippedBecause NotConnectedExchange
+        return $null
+    }
+
+    $policies = Get-MtSharingPolicy
+
+    $resultPolicies = $policies | Where-Object {`
+        $_.Enabled -and `
+        ($_.Domains -like "`*:*CalendarSharing*" -or `
+         $_.Domains -like "Anonymous:*CalendarSharing*")
+    }
+
+    $testResult = ($resultPolicies|Measure-Object).Count -eq 0
+
+    if ($testResult) {
+        $testResultMarkdown = "Well done. Your tenant does not allow uncontrolled calendar sharing.`n`n%TestResult%"
+    } else {
+        $testResultMarkdown = "Your tenant allows uncontrolled calendar sharing.`n`n%TestResult%"
+    }
+
+    $result = "| Policy Name | Test Result |`n"
+    $result += "| --- | --- |`n"
+    foreach ($item in $policies | Sort-Object -Property Name) {
+        $portalLink = "https://admin.exchange.microsoft.com/#/individualsharing/:/individualsharingdetails/$($item.ExchangeObjectId)/managedomain"
+        $itemResult = "✅ Pass"
+        if ($item.ExchangeObjectId -in $resultPolicies.ExchangeObjectId) {
+            $itemResult = "❌ Fail"
+        }
+        $result += "| [$($item.Name)]($portalLink) | $($itemResult) |`n"
+    }
+    $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $result
+
+    Add-MtTestResultDetail -Result $testResultMarkdown
+
+    return $testResult
+}
