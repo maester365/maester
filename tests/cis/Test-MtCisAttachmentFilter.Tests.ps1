c15b144941c28dbe4819c3944342ec44a07a547b
NZLostboy <ben@l33t.co.nz>
2024-09-22 15:46:19 +1200

    message update

--- tests/cis/Test-MtCisAttachmentFilter.Tests.ps1
+++ tests/cis/Test-MtCisAttachmentFilter.Tests.ps1
@@ -4,7 +4,7 @@ Describe "CIS" -Tag "CIS 2.1.2", "L1", "CIS E3 Level 1", "CIS E3", "CIS", "Secur
         $result = Test-MtCisAttachmentFilter
 
         if ($null -ne $result) {
-            $result | Should -Be $true -Because "common attachment file filter is enabled ."
+            $result | Should -Be $true -Because "the common attachment file filter is enabled."
         }
     }
 }
