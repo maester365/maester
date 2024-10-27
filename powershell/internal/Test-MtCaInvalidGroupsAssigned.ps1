8b7edee4f884b6de8e2b80ae393f9376acc781ec
Thomas Naunheim <47817884+Cloud-Architekt@users.noreply.github.com>
2024-10-27 20:17:45 +0100

    Update powershell/internal/Test-MtCaInvalidGroupsAssigned.ps1
    Co-authored-by: Fabian Bader <f-bader@users.noreply.github.com>

--- powershell/internal/Test-MtCaInvalidGroupsAssigned.ps1
+++ powershell/internal/Test-MtCaInvalidGroupsAssigned.ps1
@@ -48,7 +48,7 @@ Function Test-MtCaInvalidGroupsAssigned {
       if ( $result ) {
         $ResultDescription = "Well done! All security groups with assignment in Conditional Access are protected!"
       } else {
-        $ResultDescription = "These security groups with assignments in Conditional Access are exists anymore. Invalid groups are only visible in the policy assignment in Microsoft Graph API and not in the Portal UI."
+        $ResultDescription = "The security groups with active assignments in Conditional Access do not exist anymore. Invalid groups are only visible in the policy assignment in Microsoft Graph API and not in the Portal UI."
         $ImpactedCaGroups = "`n`n#### Impacted Conditional Access Policies`n`n | Security Group | Condition | Policy name | `n"
         $ImpactedCaGroups += "| --- | --- | --- |`n"
       }
