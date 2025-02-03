57c60643a7e873f82776374f1ffa91ba1bca8cc3
Sam Erde <samuel.erde@gmail.com>
2025-02-03 15:06:48 -0500

    Add switch for any exports. Add force parameter to overwrite existing output files.

--- powershell/public/Convert-MtJsonResultsToFlatObject.ps1
+++ powershell/public/Convert-MtJsonResultsToFlatObject.ps1
@@ -10,14 +10,20 @@
     .PARAMETER JsonFilePath
     The path of the file containing the Maester test results in JSON format.
 
+    .PARAMETER ExportCsv
+    Export the flattened object to a CSV file.
+
+    .PARAMETER ExportExcel
+    Export the flattened object to an Excel workbook using the ImportExcel module.
+
     .PARAMETER CsvFilePath
     The path of the file to export CSV data to.
 
     .PARAMETER ExcelFilePath
     The path of the file to export an Excel worksheet to.
 
-    .PARAMETER ExportExcel
-    Export the flattened object to an Excel workbook using the ImportExcel module.
+    .PARAMETER Force
+    Force the export to a CSV/XLSX file even if the file already exists.
 
     .PARAMETER Passthru
     Return the flattened object to the pipeline.
@@ -25,7 +31,7 @@
     .EXAMPLE
     Convert-MtJsonResultsToFlatObject -JsonFilePath 'C:\path\to\results.json'
 
-    Convert the Maester test results from JSON to a flattened object and then export that object to a CSV file.
+    Convert the Maester test results from JSON to a flattened object that is returned to the pipeline.
 
     .EXAMPLE
     Convert-MtJsonResultsToFlatObject -JsonFilePath 'C:\path\to\results.json' -ExportExcel
@@ -36,9 +42,10 @@
     System.Collections.Generic.List[PSObject]
 
     .NOTES
-    Due to limitations in CSV files and Excel cells, the ResultDetails property is truncated to only include the test
-    result and not details about impacted objects. The full details are still available in the JSON file.
-
+    Due to limitations in CSV files and Excel cells, the ResultDetails property is limited to 30000 characters. If the
+    test result details are longer than this, that section will be truncated and a notification will be included in its
+    place. This is most likely to happen when details about a large number of users is included in the result details.
+    The full details are still available in the JSON file and the HTML report.
     #>
     [CmdletBinding()]
     [OutputType([System.Collections.Generic.List[PSObject]])]
@@ -56,10 +63,20 @@
         [Parameter(HelpMessage = 'The path to the Excel file to which the Maester test results will be exported.')]
         [string]$ExcelFilePath = "$($JsonFilePath -replace '\.json$', '.xlsx')",
 
+        # Export the Maester test results to a CSV file.
+        [Parameter()]
+        [switch]
+        $ExportCsv,
+
         # Export the Maester test results to an Excel file.
         [Parameter()]
         [switch]
         $ExportExcel,
+
+        # Force the export to a CSV/XLSX file even if the file already exists.
+        [Parameter()]
+        [switch]
+        $Force,
 
         # Return the flattened object to the pipeline.
         [Parameter()]
@@ -67,66 +84,87 @@
         $Passthru
     )
 
-    #region ReplacementStrings
-    # Replacement strings for emoji characters and apostrophes that do not translate well to CSV files.
-    $ReplacementStrings = @{
-        'âŒ'       = ''
-        'âž¡ï¸'    = ''
-        'âœ…'       = ''
-        'youâ€™re'  = 'you are'
-        'arenâ€™nt' = 'are not'
-    } ; [void]$ReplacementStrings # Not Used Yet
-
-    [string]$TruncationFYI = 'NOTE: DETAILS ARE TRUNCATED DUE TO FIELD SIZE LIMITATIONS. PLEASE SEE THE HTML REPORT FOR FULL DETAILS.'
-    #endregion ReplacementStrings
-
-    $MaesterResults = New-Object System.Collections.Generic.List[PSObject]
-
-    $JsonData = (Get-Content -Path $JsonFilePath | ConvertFrom-Json).Tests
-    $JsonData | ForEach-Object {
-        # Truncate the ResultDetail.TestResult data if it is longer than 30000 characters.
-        if ($_.ResultDetail.TestResult.Length -gt 30000) {
-            $TestResultDetail = "$TruncationFYI`n`n$($TestResultDetail -replace '(?s)(.*?)#### Impacted resources.*?#### Remediation actions:','$1#### Remediation actions:')"
-        } else {
-            $TestResultDetail = $_.ResultDetail.TestResult
+    begin {
+        # Check for an existing CSV file.
+        if ($CsvFilePath -and (Test-Path -Path $CsvFilePath -PathType Leaf) -and -not $Force.IsPresent) {
+            throw "The specified CSV file path '$CsvFilePath' already exists. Use -Force if you want to overwrite this file."
         }
 
-        $MaesterResults.Add([PSCustomObject]@{
-                Name           = $_.Name
-                Tag            = $_.Tag -join ', '
-                Block          = $_.Block
-                Result         = $_.Result
-                Description    = $_.ResultDetail.TestDescription
-                ResultDetail   = $TestResultDetail
-                TestSkipped    = $_.ResultDetail.TestSkipped
-                SkippedReason  = $_.ResultDetail.SkippedReason
-                ErrorMessage   = $_.ErrorRecord.Exception.Message
-                HelpUrl        = $_.HelpUrl
-                TestScriptFile = [System.IO.Path]::GetFileName($_.ScriptBlockFile)
-            })
-    }
+        # Check for an existing Excel file.
+        if ($ExcelFilePath -and (Test-Path -Path $ExcelFilePath -PathType Leaf) -and -not $Force.IsPresent) {
+            throw "The specified Excel file path '$ExcelFilePath' already exists. Use -Force if you want to overwrite this file."
+        }
 
-    try {
-        $MaesterResults | Export-Csv -Path $CsvFilePath -UseQuotes Always -NoTypeInformation
-        Write-Information "Exported the Maester test results to '$CsvFilePath'." -InformationAction Continue
-    } catch {
-        Write-Error "Failed to export the Maester test results to a CSV file. $_"
-    }
+        # Replacement strings for emoji characters and apostrophes that do not translate well to CSV files.
+        [hashtable]$ReplacementStrings = @{
+            'âŒ'       = ''
+            'âž¡ï¸'    = ''
+            'âœ…'       = ''
+            'youâ€™re'  = 'you are'
+            'arenâ€™nt' = 'are not'
+        } ; [void]$ReplacementStrings # Not Used Yet
+        [string]$TruncationFYI = 'NOTE: DETAILS ARE TRUNCATED DUE TO FIELD SIZE LIMITATIONS. PLEASE SEE THE HTML REPORT FOR FULL DETAILS.'
 
-    if ($ExportExcel.IsPresent) {
-        try {
-            $MaesterResults | Export-Excel -Path $ExcelFilePath -FreezeTopRow -AutoFilter -BoldTopRow -WorksheetName 'Results'
-            Write-Information "Exported the Maester test results to '$ExcelFilePath'." -InformationAction Continue
-        } catch [System.Management.Automation.CommandNotFoundException] {
-            Write-Error "The ImportExcel module is required to export the Maester test results to an Excel file. Install the module using ``Import-Module -Name 'ImportExcel'`` and try again."
+        $MaesterResults = New-Object System.Collections.Generic.List[PSObject]
 
-        } catch {
-            Write-Error "Failed to export the Maester test results to an Excel file. $_"
+        $JsonData = (Get-Content -Path $JsonFilePath | ConvertFrom-Json).Tests
+    } #end begin
+
+    process {
+        $JsonData | ForEach-Object {
+
+            # Truncate the ResultDetail.TestResult data if it is longer than 30000 characters.
+            if ($_.ResultDetail.TestResult.Length -gt 30000) {
+                $TestResultDetail = "$TruncationFYI`n`n$($TestResultDetail -replace '(?s)(.*?)#### Impacted resources.*?#### Remediation actions:','$1#### Remediation actions:')"
+            } else {
+                $TestResultDetail = $_.ResultDetail.TestResult
+            }
+
+            # Add the flattened object to the MaesterResults list.
+            $MaesterResults.Add([PSCustomObject]@{
+                    Name           = $_.Name
+                    Tag            = $_.Tag -join ', '
+                    Block          = $_.Block
+                    Result         = $_.Result
+                    Description    = $_.ResultDetail.TestDescription
+                    ResultDetail   = $TestResultDetail
+                    TestSkipped    = $_.ResultDetail.TestSkipped
+                    SkippedReason  = $_.ResultDetail.SkippedReason
+                    ErrorMessage   = $_.ErrorRecord.Exception.Message
+                    HelpUrl        = $_.HelpUrl
+                    TestScriptFile = [System.IO.Path]::GetFileName($_.ScriptBlockFile)
+                })
+        }
+
+        # Export the MaesterResults list to a CSV if requested.
+        if ($ExportCsv.IsPresent) {
+            try {
+                $MaesterResults | Export-Csv -Path $CsvFilePath -UseQuotes Always -NoTypeInformation
+                Write-Information "Exported the Maester test results to '$CsvFilePath'." -InformationAction Continue
+            } catch {
+                Write-Error "Failed to export the Maester test results to a CSV file. $_"
+            }
+        }
+
+        # Export the MaesterResults list to an Excel file if requested.
+        if ($ExportExcel.IsPresent) {
+            try {
+                $MaesterResults | Export-Excel -Path $ExcelFilePath -FreezeTopRow -AutoFilter -BoldTopRow -WorksheetName 'Results'
+                Write-Information "Exported the Maester test results to '$ExcelFilePath'." -InformationAction Continue
+            } catch [System.Management.Automation.CommandNotFoundException] {
+                Write-Error "The ImportExcel module is required to export the Maester test results to an Excel file. Install the module using ``Import-Module -Name 'ImportExcel'`` and try again."
+
+            } catch {
+                Write-Error "Failed to export the Maester test results to an Excel file. $_"
+            }
         }
     }
 
-    if ($Passthru.IsPresent) {
-        $MaesterResults
+    end {
+        # Return the flattened object to the pipeline if requested or if no export is requested.
+        if ($Passthru.IsPresent -or (-not $ExportCsv.IsPresent -and -not $ExportExcel.IsPresent)) {
+            $MaesterResults
+        }
     }
 
 } #end function Convert-MtJsonResultsToFlatObject
