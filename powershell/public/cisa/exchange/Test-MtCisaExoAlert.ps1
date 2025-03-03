993ca7c4eafd13a6791a79ecc3cbf2da99e771ed
Thomas S. Schmidt <thosc@tdc.dk>
2025-03-03 21:00:23 +0100

    Updated to improved Get-MtLicenseInformation MdoV2 logic in Test-MtCisaExoAlert

--- powershell/public/cisa/exchange/Test-MtCisaExoAlert.ps1
+++ powershell/public/cisa/exchange/Test-MtCisaExoAlert.ps1
@@ -24,25 +24,23 @@ function Test-MtCisaExoAlert {
     }elseif(!(Test-MtConnection SecurityCompliance)){
         Add-MtTestResultDetail -SkippedBecause NotConnectedSecurityCompliance
         return $null
-    }elseif($null -eq (Get-MtLicenseInformation -Product Mdo)){
-        Add-MtTestResultDetail -SkippedBecause NotLicensedMdo
+    } elseif ("P1" -notin (Get-MtLicenseInformation -Product MdoV2)) {
+        Add-MtTestResultDetail -SkippedBecause NotLicensedMdoP1
         return $null
     }
 
     $alerts = Get-MtExo -Request ProtectionAlert
 
-    $cisaAlerts = @(
-        "be215649-fba8-4339-9ddd-05991a43b948", #Suspicious email sending patterns detected
-        "8bb9c6c8-dc12-40e1-5bb8-08da05b13393", #Suspicious connector activity
-        "bfd48f06-0865-41a6-85ff-adb746423ebf", #Suspicious Email Forwarding Activity
-        "37a4e852-e711-45ca-b0f4-b076bae3adfd", #Messages have been delayed
-        "5ed2d687-9bd3-49e7-9b56-b7dc0d9af5cb", #Tenant restricted from sending unprovisioned email
-        "a7032ff5-7eee-412b-805b-d1295c7e0932", #Tenant restricted from sending email
-        "a74bb32a-541b-47fb-adfd-f8c62ce3d59b"  #A potentially malicious URL click was detected
-    )
-
-    $filterAlerts = $alerts | Where-Object { `
-        $_.ExchangeObjectId -in $cisaAlerts
+    $cisaAlerts = @{
+        'be215649-fba8-4339-9ddd-05991a43b948' = 'Suspicious email sending patterns detected'
+        '8bb9c6c8-dc12-40e1-5bb8-08da05b13393' = 'Suspicious connector activity'
+        'bfd48f06-0865-41a6-85ff-adb746423ebf' = 'Suspicious Email Forwarding Activity'
+        '37a4e852-e711-45ca-b0f4-b076bae3adfd' = 'Messages have been delayed'
+        '5ed2d687-9bd3-49e7-9b56-b7dc0d9af5cb' = 'Tenant restricted from sending unprovisioned email'
+        'a7032ff5-7eee-412b-805b-d1295c7e0932' = 'Tenant restricted from sending email'
+    }
+    if ("P2" -in (Get-MtLicenseInformation -Product MdoV2)) {
+        $cisaAlerts['a74bb32a-541b-47fb-adfd-f8c62ce3d59b'] = 'A potentially malicious URL click was detected'
     }
 
     $resultAlerts = $alerts | Where-Object { `
