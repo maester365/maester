d3cf72100db7c482938c0029f67cfdec245dbea2
Thomas S. Schmidt <thosc@tdc.dk>
2025-03-03 19:17:35 +0100

    Show 50 items instead of only 6 in Test-MtCis365PublicGroup. This will help in large tenants to get a proper overview without being unlimited.

--- powershell/public/cis/Test-MtCis365PublicGroup.ps1
+++ powershell/public/cis/Test-MtCis365PublicGroup.ps1
@@ -45,14 +45,14 @@ function Test-MtCis365PublicGroup {
     foreach ($item in $result) {
         $itemCount += 1
         $itemResult = "❌ Fail"
-        # We are restricting the table output to 6 below as it could be extremely large
-        if ($itemCount -lt 7) {
+        # We are restricting the table output to 50 below as it could be extremely large
+        if ($itemCount -lt 51) {
             $resultMd += "| $($item.displayName) | $($itemResult) |`n"
         }
     }
     # Add a limited results message if more than 6 results are returned
-    if ($itemCount -gt 6) {
-        $resultMd += "Results limited to 6`n"
+    if ($itemCount -gt 50) {
+        $resultMd += "Results limited to 50`n"
     }
 
     $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $resultMd
