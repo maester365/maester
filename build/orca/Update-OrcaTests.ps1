debd67e0137df742d61c7748324fac4ffc3e1c32
Thomas S. Schmidt <thosc@tdc.dk>
2025-03-04 09:06:19 +0100

    Update control capture to improved regular expression

--- build/orca/Update-OrcaTests.ps1
+++ build/orca/Update-OrcaTests.ps1
@@ -147,7 +147,7 @@ foreach($file in $testFiles){
     $name = [regex]::Match($content.content, "this\.name=([\'\`"])(?'capture'.*)\1", $option)
     $pass = [regex]::Match($content.content, "this\.passText=([\'\`"])(?'capture'.*)\1", $option)
     $fail = [regex]::Match($content.content, "this\.failrecommendation=([\'\`"])(?'capture'.*)\1", $option)
-    $control = [regex]::Match($content.content,"this.control.*[\'\`"](?'capture'.*)[\'\`"]",$option)
+    $control = [regex]::Match($content.content, "this\.control=([\'\`"])(?'capture'.*)\1", $option)
     $area = [regex]::Match($content.content,"this.area.*[\'\`"](?'capture'.*)[\'\`"]",$option)
     $content.name = $name.Groups['capture'].Value
     $content.pass = $pass.Groups['capture'].Value
