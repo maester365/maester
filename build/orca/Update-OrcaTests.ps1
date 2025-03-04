b32e43a6d14d7291f21909b862f5b873360e9b8a
Thomas S. Schmidt <thosc@tdc.dk>
2025-03-04 10:05:46 +0100

    Added exception output before throw as suggested by Gemini bot

--- build/orca/Update-OrcaTests.ps1
+++ build/orca/Update-OrcaTests.ps1
@@ -236,6 +236,7 @@ function Test-$($content.func){
     try { # Handle "SkipInReport" which has a continue statement that makes this function exit unexpectedly
         `$obj.Run(`$Collection)
     } catch {
+        Write-Error "An error occurred during $($content.func): `$(`$_.Exception.Message)"
         throw
     } finally {
         if(`$obj.SkipInReport) {
