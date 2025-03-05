83b2c5d0b7e4f470fbbd05ed54dd07bf24fbdf99
Merill Fernando <merill@merill.net>
2025-03-06 10:08:39 +1100

    Fixed name of cmdlet to call the right one.

--- tests/cis/Test-MtCisAttachmentFilterComprehensive.Tests.ps1
+++ tests/cis/Test-MtCisAttachmentFilterComprehensive.Tests.ps1
@@ -1,7 +1,7 @@
 Describe "CIS" -Tag "CIS 2.1.14", "L2", "CIS E3 Level 2", "CIS E3", "CIS", "Security", "All", "CIS M365 v3.1.0" {
     It "CIS 2.1.14 (L2) Ensure comprehensive attachment filtering is applied" {
 
-        $result = Test-MtCisAttachmentFilterTypeList
+        $result = Test-MtCisAttachmentFilterComprehensive
 
         if ($null -ne $result) {
             $result | Should -Be $true -Because "the default malware filter policy did not have comprehensive attachment filtering applied."
