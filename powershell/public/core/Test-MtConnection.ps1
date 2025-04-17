498510ca82e4b8290e9c16b055051dd22633172a
Sam Erde <samuel.erde@gmail.com>
2025-04-16 23:39:52 -0400

    Add detailed output about connections

--- powershell/public/core/Test-MtConnection.ps1
+++ powershell/public/core/Test-MtConnection.ps1
@@ -6,88 +6,151 @@
     .DESCRIPTION
     Tests the connection for each service and returns $true if the session is connected to the specified service.
 
-    .EXAMPLE
-    Test-MtConnection
+    .PARAMETER Service
+    The service to check the connection for. Valid values are 'All', 'Azure', 'ExchangeOnline', 'Graph', 'SecurityCompliance' (or 'EOP'), and 'Teams'. Default is 'All'.
 
-    Checks if the current session is connected to Microsoft Graph.
+    .PARAMETER Details
+    Return the full details of all connections instead of just a boolean value.
 
     .EXAMPLE
     Test-MtConnection -Service All
 
-    Checks if the current session is connected to all services including Azure, Microsoft Graph, Exchange Online, Exchange Online Protection (via IPPSSession), and Microsoft Teams.
+    Checks if the current session is connected to all services including Azure, Microsoft Graph, Exchange Online, Exchange Online Protection (SecurityCompliance), and Microsoft Teams. Returns a Boolean value.
+
+    .EXAMPLE
+    Test-MtConnection -Details
+
+    Checks if the current session is connected to all services including Azure, Microsoft Graph, Exchange Online, Exchange Online Protection (SecurityCompliance), and Microsoft Teams. Returns a custom object that contains the connection details for all services.
+
+    .EXAMPLE
+    Test-MtConnection -Service Azure
+
+    Checks if the current session is connected to Azure and returns a Boolean result.
 
     .LINK
     https://maester.dev/docs/commands/Test-MtConnection
-#>
+    #>
     [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', 'AvoidUsingWriteHost', Justification = 'Sending colorful output to host in addition to rich object output.')]
     param(
         # Checks if the current session is connected to the specified service
-        [ValidateSet('All', 'Azure', 'ExchangeOnline', 'Graph', 'SecurityCompliance', 'Teams')]
-        [Parameter(Position = 0, Mandatory = $false)]
-        [string[]]$Service = 'All'
+        [ValidateSet('All', 'Azure', 'ExchangeOnline', 'EOP', 'Graph', 'SecurityCompliance', 'Teams')]
+        [Parameter(Position = 0)]
+        [string[]]$Service = 'All',
+
+        # Return the full details of the connections
+        [Parameter()]
+        [switch]$Details
     )
 
-    $ConnectionState = $true
+    begin {
+        $MtConnections = [PSCustomObject]@{
+            PSTypeName  = 'Maester.Connections'
+            Azure = $null
+            Graph = $null
+            ExchangeOnline = $null
+            ExchangeOnlineProtection = $null
+            Teams = $null
+            AllConnected = $false
+        }
 
-    if ($Service -contains 'Azure' -or $Service -contains 'All') {
-        $IsConnected = $false
-        try {
-            $IsConnected = $null -ne (Get-AzContext -ErrorAction SilentlyContinue)
-            # Validate that the credentials are still valid
-            if($IsConnected) {
-                Invoke-AzRestMethod -Method GET -Path 'subscriptions?api-version=2022-12-01' | Out-Null
-            }
-        } catch {
+        # This can potentially be replaced by $MtConnections.AllConnected but all functions that reference this function would need to be updated.
+        $ConnectionState = $true
+    }
+
+    process {
+        #region Azure
+        if ($Service -contains 'Azure' -or $Service -contains 'All') {
             $IsConnected = $false
-            Write-Debug "Azure: $false"
+            try {
+                $MtConnections.Azure = Get-AzContext
+                $IsConnected = $null -ne ($MtConnections.Azure)
+                # Validate that the credentials are still valid
+                if ($IsConnected) {
+                    Invoke-AzRestMethod -Method GET -Path 'subscriptions?api-version=2022-12-01' | Out-Null
+                }
+            } catch {
+                $IsConnected = $false
+                Write-Debug "Azure: $false"
+            }
+            Write-Verbose "Azure: $IsConnected"
+            if (!$IsConnected) { $ConnectionState = $false }
         }
-        Write-Verbose "Azure: $IsConnected"
-        if (!$IsConnected) { $ConnectionState = $false }
+        #endregion Azure
+
+        #region Graph
+        if ($Service -contains 'Graph' -or $Service -contains 'All') {
+            $IsConnected = $false
+            try {
+                $MtConnections.Graph = Get-MgContext
+                $IsConnected = $null -ne ($MtConnections.Graph)
+            } catch {
+                # Re-test
+                Write-Debug "Graph: $false"
+            }
+            Write-Verbose "Graph: $IsConnected"
+            if (!$IsConnected) { $ConnectionState = $false }
+        }
+        # To Do: Add checks for required scopes.
+        #endregion Graph
+
+        #region Exchange Online
+        if ($Service -contains 'ExchangeOnline' -or $Service -contains 'All') {
+            $IsConnected = $false
+            try {
+                $MtConnections.ExchangeOnline = (Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' -and -not $_.IsEopSession })
+                $IsConnected = $null -ne ($MtConnections.ExchangeOnline)
+            } catch {
+                Write-Debug "Exchange Online: $false"
+            }
+            Write-Verbose "Exchange Online: $IsConnected"
+            if (!$IsConnected) { $ConnectionState = $false }
+        }
+        #endregion Exchange Online
+
+        #region Exchange Online Protection (EOP)
+        if (($Service -contains 'SecurityCompliance' -or $Service -contains 'EOP') -or $Service -contains 'All') {
+            $IsConnected = $false
+            try {
+                $MtConnections.ExchangeOnlineProtection = (Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' -and $_.IsEopSession })
+                $IsConnected = $null -ne ($MtConnections.ExchangeOnlineProtection)
+            } catch {
+                # Re-test
+                Write-Debug "Security & Compliance: $false"
+            }
+            Write-Verbose "Security & Compliance: $IsConnected"
+            if (!$IsConnected) { $ConnectionState = $false }
+        }
+        #endregion Exchange Online Protection (EOP)
+
+        #region Teams
+        if ($Service -contains 'Teams' -or $Service -contains 'All') {
+            $IsConnected = $false
+            try {
+                $MtConnections.Teams = Get-CsTenant
+                $IsConnected = $null -ne ($MtConnections.Teams)
+            } catch {
+                # Re-test
+                Write-Debug "Teams: $false"
+            }
+            Write-Verbose "Teams: $IsConnected"
+            if (!$IsConnected) { $ConnectionState = $false }
+        }
+        #endregion Teams
+
+        if ($IsConnected) {
+            $MtConnections.AllConnected = $true
+        }
+
     }
 
-    if ($Service -contains 'Graph' -or $Service -contains 'All') {
-        $IsConnected = $false
-        try {
-            $IsConnected = $null -ne (Get-MgContext -ErrorAction SilentlyContinue)
-        } catch {
-            Write-Debug "Graph: $false"
+    end {
+        if ($Details.IsPresent) {
+            # Return the full details of all active connections.
+            $MtConnections
+        } else {
+            # Only return a boolean value indicating if all sessions are connected.
+            $ConnectionState
         }
-        Write-Verbose "Graph: $IsConnected"
-        if (!$IsConnected) { $ConnectionState = $false }
     }
-
-    if ($Service -contains 'ExchangeOnline' -or $Service -contains 'All') {
-        $IsConnected = $false
-        try {
-            $IsConnected = $null -ne ((Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' }))
-        } catch {
-            Write-Debug "Exchange Online: $false"
-        }
-        Write-Verbose "Exchange Online: $IsConnected"
-        if (!$IsConnected) { $ConnectionState = $false }
-    }
-
-    if ($Service -contains 'SecurityCompliance' -or $Service -contains 'All') {
-        $IsConnected = $false
-        try {
-            $IsConnected = $null -ne ((Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' -and $_.IsEopSession }))
-        } catch {
-            Write-Debug "Security & Compliance: $false"
-        }
-        Write-Verbose "Security & Compliance: $IsConnected"
-        if (!$IsConnected) { $ConnectionState = $false }
-    }
-
-    if ($Service -contains 'Teams' -or $Service -contains 'All') {
-        $IsConnected = $false
-        try {
-            $IsConnected = $null -ne (Get-CsTenant -ErrorAction SilentlyContinue)
-        } catch {
-            Write-Debug "Teams: $false"
-        }
-        Write-Verbose "Teams: $IsConnected"
-        if (!$IsConnected) { $ConnectionState = $false }
-    }
-
-    Write-Output $ConnectionState
 }
