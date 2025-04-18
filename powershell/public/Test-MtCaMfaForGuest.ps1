eb8c9dbebf193a4e26053e68e283df758bd4242e
Merill Fernando <1288081+merill@users.noreply.github.com>
2025-04-18 12:45:17 +1000

    Fixes #804  Handle tenants with All Users MFA for Guest check

--- powershell/public/Test-MtCaMfaForGuest.ps1
+++ powershell/public/Test-MtCaMfaForGuest.ps1
@@ -35,23 +35,27 @@ function Test-MtCaMfaForGuest {
     $result = $false
     foreach ($policy in $policies) {
         try {
-            # Check if all guest types are present in the policy and compare with the known count of the guest types
+            # Check if all guest types are present in the policy
             $AllGuestTypesPresent = ( Compare-Object -ReferenceObject $GuestTypes -DifferenceObject ( $policy.conditions.users.includeGuestsOrExternalUsers.guestOrExternalUserTypes -split ',') -IncludeEqual -ExcludeDifferent -PassThru | Measure-Object | Select-Object -ExpandProperty Count ) -eq $GuestTypes.Count
         } catch {
             $AllGuestTypesPresent = $false
         }
-        if ( ( $policy.grantcontrols.builtincontrols -contains 'mfa' `
-                    -or $policy.grantcontrols.authenticationStrength.requirementsSatisfied -contains 'mfa' ) `
-                -and ( $policy.conditions.users.includeUsers -eq "GuestsOrExternalUsers" `
-                    -or $AllGuestTypesPresent ) `
-                -and $policy.conditions.applications.includeApplications -eq "All" `
-        ) {
+
+        # Simplified logic for checking MFA requirements
+        $RequiresMfa = $policy.grantcontrols.builtincontrols -contains 'mfa' -or $policy.grantcontrols.authenticationStrength.requirementsSatisfied -contains 'mfa'
+        $AppliesToGuests = $policy.conditions.users.includeUsers -contains "GuestsOrExternalUsers" -or $AllGuestTypesPresent
+        $AppliesToAllUsers = $policy.conditions.users.includeUsers -contains "All"
+        $AppliesToAllApps = $policy.conditions.applications.includeApplications -contains "All"
+        $ExcludesAnyGuests = $policy.conditions.users.excludeGuestsOrExternalUsers -ne $null
+
+        if ($RequiresMfa -and $AppliesToAllApps -and -not $ExcludesAnyGuests -and ($AppliesToGuests -or $AppliesToAllUsers)) {
             $result = $true
             $currentresult = $true
             $policiesResult.Add($policy) | Out-Null
         } else {
             $currentresult = $false
         }
+
         Write-Verbose "$($policy.displayName) - $currentresult"
     }
 
---
+++ powershell/tests/functions/Test-MtCaMfaForGuest.Tests.ps1
@@ -0,0 +1,203 @@
+﻿
+Describe 'Test-MtCaMfaForGuest' {
+    BeforeAll {
+        Import-Module $PSScriptRoot/../../Maester.psd1 -Force
+        Mock -ModuleName Maester Get-MtLicenseInformation { return "P1" }
+
+        function Get-AllUserMfaPolicyNoExcludeGuest {
+            $policyJson = @"
+[
+  {
+    "state": "enabled",
+    "conditions": {
+      "clientAppTypes": [
+        "all"
+      ],
+      "applications": {
+        "includeApplications": [
+          "All"
+        ],
+      },
+      "users": {
+        "includeUsers": [
+          "All"
+        ],
+      }
+    },
+    "grantControls": {
+      "operator": "OR",
+      "builtInControls": [
+        "mfa"
+      ],
+    }
+  }
+]
+"@
+            return $policyJson | ConvertFrom-Json
+        }
+
+        function Get-AllUserMfaPolicyExcludeGuest {
+            $policyJson = @"
+[
+  {
+    "state": "enabled",
+    "conditions": {
+      "clientAppTypes": [
+        "all"
+      ],
+      "applications": {
+        "includeApplications": [
+          "All"
+        ]
+      },
+      "users": {
+        "excludeUsers": [
+          "513f3db2-044c-41be-af14-431bf88a2b3e"
+        ],
+        "includeGuestsOrExternalUsers": {
+          "guestOrExternalUserTypes": "internalGuest,b2bCollaborationGuest,b2bCollaborationMember,b2bDirectConnectUser,otherExternalUser,serviceProvider",
+          "externalTenants": {
+            "@odata.type": "#microsoft.graph.conditionalAccessAllExternalTenants",
+            "membershipKind": "all"
+          }
+        },
+        "excludeGuestsOrExternalUsers": {
+          "guestOrExternalUserTypes": "b2bCollaborationGuest",
+          "externalTenants": {
+            "@odata.type": "#microsoft.graph.conditionalAccessAllExternalTenants",
+            "membershipKind": "all"
+          }
+        }
+      }
+    },
+    "grantControls": {
+      "operator": "OR",
+      "builtInControls": [
+        "mfa"
+      ],
+    }
+  }
+]
+"@
+            return $policyJson | ConvertFrom-Json
+        }
+
+
+        function Get-GuestMfaPolicyNoExcludeGuest {
+            $policyJson = @"
+[
+  {
+    "state": "enabled",
+    "conditions": {
+      "clientAppTypes": [
+        "all"
+      ],
+      "applications": {
+        "includeApplications": [
+          "All"
+        ]
+      },
+      "users": {
+        "includeGuestsOrExternalUsers": {
+          "guestOrExternalUserTypes": "internalGuest,b2bCollaborationGuest,b2bCollaborationMember,b2bDirectConnectUser,otherExternalUser,serviceProvider",
+          "externalTenants": {
+            "@odata.type": "#microsoft.graph.conditionalAccessAllExternalTenants",
+            "membershipKind": "all"
+          }
+        },
+        "excludeGuestsOrExternalUsers": null,
+      }
+    },
+    "grantControls": {
+      "operator": "OR",
+      "builtInControls": [
+        "mfa"
+      ]
+    }
+  }
+]
+"@
+            return $policyJson | ConvertFrom-Json
+        }
+
+        function Get-GuestMfaPolicyExcludeGuest {
+            $policyJson = @"
+[
+  {
+    "state": "enabled",
+    "conditions": {
+      "clientAppTypes": [
+        "all"
+      ],
+      "applications": {
+        "includeApplications": [
+          "All"
+        ]
+      },
+      "users": {
+        "includeGuestsOrExternalUsers": {
+          "guestOrExternalUserTypes": "internalGuest,b2bCollaborationGuest,b2bCollaborationMember,b2bDirectConnectUser,otherExternalUser,serviceProvider",
+          "externalTenants": {
+            "@odata.type": "#microsoft.graph.conditionalAccessAllExternalTenants",
+            "membershipKind": "all"
+          }
+        },
+        "excludeGuestsOrExternalUsers": {
+          "guestOrExternalUserTypes": "b2bCollaborationGuest",
+          "externalTenants": {
+            "@odata.type": "#microsoft.graph.conditionalAccessAllExternalTenants",
+            "membershipKind": "all"
+          }
+        }
+      }
+    },
+    "grantControls": {
+      "operator": "OR",
+      "builtInControls": [
+        "mfa"
+      ]
+    }
+  }
+]
+"@
+            return $policyJson | ConvertFrom-Json
+        }
+    }
+
+    Context "CA: MFA for Guest" {
+
+        It 'MFA for All users should pass even if not targeting guests' {
+            $policy = Get-AllUserMfaPolicyNoExcludeGuest
+
+            Mock -ModuleName Maester Get-MtConditionalAccessPolicy { return $policy }
+
+            Test-MtCaMfaForGuest | Should -BeTrue
+        }
+
+        It 'MFA for All users that excludes any guest type should fail' {
+            $policy = Get-AllUserMfaPolicyExcludeGuest
+
+            Mock -ModuleName Maester Get-MtConditionalAccessPolicy { return $policy }
+
+            Test-MtCaMfaForGuest | Should -BeFalse
+        }
+
+        It 'MFA for Guests should pass' {
+            $policy = Get-GuestMfaPolicyNoExcludeGuest
+
+            Mock -ModuleName Maester Get-MtConditionalAccessPolicy { return $policy }
+
+            Test-MtCaMfaForGuest | Should -BeTrue
+        }
+
+        It 'MFA for Guests that excludes any guest type should fail' {
+            $policy = Get-GuestMfaPolicyExcludeGuest
+
+            Mock -ModuleName Maester Get-MtConditionalAccessPolicy { return $policy }
+
+            Test-MtCaMfaForGuest | Should -BeFalse
+        }
+
+    }
+}
+
