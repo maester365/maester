d0210797d144dedeec83817b553489ed82398288
Sam Erde <samuel.erde@gmail.com>
2025-05-05 08:42:04 -0400

    feat: ✨ Add Show-MaesterWelcome script for getting started guidance
    * Introduced `Show-MaesterWelcome.ps1` to provide users with a clear starting point.
    * Displays essential commands for installing tests and connecting to Maester.

---
+++ powershell/Show-MaesterWelcome.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '', Justification = 'Colors are beautiful')]
+param ()
+
+Show-MtLogo
+Write-Host "    To get started, install Maester tests and connect before running Maester:`n" -ForegroundColor Yellow
+Write-Host "`tmd 'Maester-Tests'           " -ForegroundColor Black -BackgroundColor Gray -NoNewline
+Write-Host ''
+Write-Host "`tcd 'Maester-Tests'           " -ForegroundColor Black -BackgroundColor Gray -NoNewline
+Write-Host ''
+Write-Host "`tInstall-MtTests              " -ForegroundColor Black -BackgroundColor Gray -NoNewline
+Write-Host ''
+Write-Host "`tConnect-Maester -Service All " -ForegroundColor Black -BackgroundColor Gray -NoNewline
+Write-Host ''
+Write-Host "`tInvoke-Maester               " -ForegroundColor Black -BackgroundColor Gray -NoNewline
+Write-Host ''
+Write-Host "`n    See https://maester.dev for more info.🔥`n" -ForegroundColor Yellow
