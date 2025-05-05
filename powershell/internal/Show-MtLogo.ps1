af86cf3e58ceff9181452492cf98728c656993ee
Sam Erde <samuel.erde@gmail.com>
2025-05-05 08:41:36 -0400

    feat: ✨ Add Show-MtLogo function to display module logo
    * Introduced `Show-MtLogo` function to output the module logo in ASCII art.
    * The logo includes the module version from the manifest.

---
+++ powershell/internal/Show-MtLogo.ps1
@@ -0,0 +1,21 @@
+﻿function Show-MtLogo {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '', Justification = 'Colors are beautiful')]
+    [OutputType([string])]
+    param ()
+
+    $Version = (Import-PowerShellDataFile -Path "$PSScriptRoot/../Maester.psd1").ModuleVersion
+    # ASCII Art using style "ANSI Shadow"
+    $Logo = @"
+
+    ███╗   ███╗ █████╗ ███████╗███████╗████████╗███████╗██████╗
+    ████╗ ████║██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+    ██╔████╔██║███████║█████╗  ███████╗   ██║   █████╗  ██████╔╝
+    ██║╚██╔╝██║██╔══██║██╔══╝  ╚════██║   ██║   ██╔══╝  ██╔══██╗
+    ██║ ╚═╝ ██║██║  ██║███████╗███████║   ██║   ███████╗██║  ██║
+    ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝ v$Version
+
+"@
+
+    Write-Host $Logo -ForegroundColor Green
+}
