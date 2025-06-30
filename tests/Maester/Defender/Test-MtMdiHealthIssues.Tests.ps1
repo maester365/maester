27de5dcd903fd25d63f7204a067df54cbc441fdb
Thomas Naunheim <47817884+Cloud-Architekt@users.noreply.github.com>
2025-06-30 05:28:20 +0000

    Changed Maester ID and minor changes on text and reused variables

--- tests/Maester/Defender/Test-MtMdiHealthIssues.Tests.ps1
+++ tests/Maester/Defender/Test-MtMdiHealthIssues.Tests.ps1
@@ -1,4 +1,6 @@
 BeforeDiscovery {
+    $checkid = "MT.1059"
+
     try {
         $MdiSecurityApiError = $null
         $MdiAllHealthIssues = Invoke-MtGraphRequest -DisableCache -ApiVersion beta -RelativeUri 'security/identities/healthIssues' -OutputType Hashtable -ErrorVariable MdiSecurityApiError
@@ -8,44 +10,44 @@ BeforeDiscovery {
     }
 
     if ($MdiSecurityApiError -match "Tenant is not onboarded to Microsoft Defender for Identity") {
-        Add-MtTestResultDetail -TestName "MT.1059: MDI Health issues should be resolved" -Severity "Medium" -Description "This test checks for health issues in Microsoft Defender for Identity. The tenant is not onboarded to Microsoft Defender for Identity, so no health issues can be retrieved." -SkippedBecause 'Custom' -SkippedCustomReason 'Tenant is not onboarded to Microsoft Defender for Identity'
+        Add-MtTestResultDetail -TestName "$($checkid): MDI Health issues should be resolved" -Severity "Medium" -Description "This test checks for health issues in Microsoft Defender for Identity. The tenant is not onboarded to Microsoft Defender for Identity, so no health issues can be retrieved." -SkippedBecause 'Custom' -SkippedCustomReason 'Tenant is not onboarded to Microsoft Defender for Identity'
         return $null
     } elseif (($MdiAllHealthIssues | Where-Object { $_.status -ne "closed" } | Measure-Object) -eq 0) {
-        Add-MtTestResultDetail -TestName "MT.1059: MDI Health issues should be resolved" -Severity "Medium" -Description "This test checks for health issues in Microsoft Defender for Identity" -SkippedBecause "Custom" -SkippedCustomReason "No health issues found"
+        Add-MtTestResultDetail -TestName "$($checkid): MDI Health issues should be resolved" -Severity "Medium" -Description "This test checks for health issues in Microsoft Defender for Identity" -SkippedBecause "Custom" -SkippedCustomReason "No health issues found"
         return $null
     }
 
-$MdiHealthIssues = [System.Collections.Generic.List[Object]]::new()
+    $MdiHealthIssues = [System.Collections.Generic.List[Object]]::new()
 
-# Add domainNames and sensorDNSNames as string properties to identify unique health issues
-$MdiAllHealthIssues | ForEach-Object {
-    $_ | Add-Member -NotePropertyName 'domainNamesString' -NotePropertyValue ($_.domainNames -join ',') -Force
-    $_ | Add-Member -NotePropertyName 'sensorDNSNamesString' -NotePropertyValue ($_.sensorDNSNames -join ',') -Force
-}
-
-# Get unique health issues (duplicated entries will be created when status of an issue has been changed)
-$MdiAllHealthIssues | Group-Object -Property displayName, domainNamesString, sensorDNSNamesString | ForEach-Object {
-    $UniqueHealthIssue = $_.Group | Sort-Object -Property createdDateTime | Select-Object -First 1
-
-    # Add the displayName to the health issue to avoid confusion of same health issue name
-    if ($UniqueHealthIssue.displayName -eq "Sensor stopped communicating") {
-        $UniqueHealthIssue.displayName = $UniqueHealthIssue.displayName + " - " + $UniqueHealthIssue.sensorDNSNames
+    # Add domainNames and sensorDNSNames as string properties to identify unique health issues
+    $MdiAllHealthIssues | ForEach-Object {
+        $_ | Add-Member -NotePropertyName 'domainNamesString' -NotePropertyValue ($_.domainNames -join ',') -Force
+        $_ | Add-Member -NotePropertyName 'sensorDNSNamesString' -NotePropertyValue ($_.sensorDNSNames -join ',') -Force
     }
-    $MdiHealthIssues.Add($UniqueHealthIssue) | Out-Null
-}
 
-$MdiHealthActiveIssues = $MdiHealthIssues | Where-Object { $_.status -ne "closed" }
+    # Get unique health issues (duplicated entries will be created when status of an issue has been changed)
+    $MdiAllHealthIssues | Group-Object -Property displayName, domainNamesString, sensorDNSNamesString | ForEach-Object {
+        $UniqueHealthIssue = $_.Group | Sort-Object -Property createdDateTime | Select-Object -First 1
+
+        # Add the displayName to the health issue to avoid confusion of same health issue name
+        if ($UniqueHealthIssue.displayName -eq "Sensor stopped communicating") {
+            $UniqueHealthIssue.displayName = $UniqueHealthIssue.displayName + " - " + $UniqueHealthIssue.sensorDNSNames
+        }
+        $MdiHealthIssues.Add($UniqueHealthIssue) | Out-Null
+    }
+
+    $MdiHealthActiveIssues = $MdiHealthIssues | Where-Object { $_.status -ne "closed" }
 }
 
 Describe "Defender for Identity health issues" -Tag "Maester", "Defender", "Security", "All", "MDI" -ForEach $MdiHealthActiveIssues {
-    It "MT.1058: MDI Health Issues - <displayName>. See https://maester.dev/docs/tests/MT.1058" -Tag "MT.1058", "Severity:Medium", $displayName {
+    It "MT.1059: MDI Health Issues - <displayName>. See https://maester.dev/docs/tests/MT.1058" -Tag "MT.1058", "Severity:Medium", $displayName {
 
         $issueUrl = "https://security.microsoft.com/identities/health-issues"
         $recommendationLinkMd = "`n`n➡️ Open [Health issue - $displayName]($issueUrl) in the Microsoft Defender portal."
-        $testTitle = "MT.1058.$($id): MDI Health Issues - $displayName"
+        $testTitle = "MT.1059.$($id): MDI Health Issues - $displayName"
 
         if ( $status -match "dismissed" -or $status -match "suppressed" ) {
-            Add-MtTestResultDetail -TestTitle $testTitle -Description $description -SkippedBecause Custom -SkippedCustomReason "This health issue has been **Suppressed** by an administrator.`n`nIf this issue is valid for your MDI instance you can change it's state from **suppressed** to **Re-open**. $recommendationLinkMd"
+            Add-MtTestResultDetail -TestTitle $testTitle -Description $description -SkippedBecause Custom -SkippedCustomReason "This health issue has been **suppressed** by an administrator.`n`nIf this issue is valid for your MDI instance, you can change it's state from **suppressed** to **Re-open**. $recommendationLinkMd"
             return $null
         }
 
