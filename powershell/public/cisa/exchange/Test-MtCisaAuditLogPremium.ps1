36b124bf5ed630da626562ab8fa6a61692dedbb6
Kent J. <kentbjoh@users.noreply.github.com>
2025-06-30 22:47:53 +0200

    fix&improve MtCisaAuditLogPremium
    fix&improve MtCisaAuditLogPremium

--- powershell/public/cisa/exchange/Test-MtCisaAuditLogPremium.ps1
+++ powershell/public/cisa/exchange/Test-MtCisaAuditLogPremium.ps1
@@ -30,25 +30,26 @@ function Test-MtCisaAuditLogPremium {
     }
 
     Write-Verbose "In tenants with a substantial number of mailboxes this test may take time"
-    $mailboxes = Get-EXOMailbox -Properties AuditOwner
+    $mailboxes = Get-EXOMailbox -ResultSize Unlimited -PropertySets Audit
 
     $resultMailboxes = $mailboxes | Where-Object { `
         $_.AuditOwner -notcontains "SearchQueryInitiated"
     }
 
-    $testResult = ($resultMailboxes|Measure-Object).Count -ge 1
+    $testResult = ($resultMailboxes|Measure-Object).Count -eq 0
 
     $portalLink = "https://purview.microsoft.com/audit/auditsearch"
     $passResult = "✅ Pass"
     $failResult = "❌ Fail"
 
     if ($testResult) {
-        $testResultMarkdown = "Well done. Your tenant has [SearchQueryInitiated audit log enabled]($portalLink).`n`n%TestResult%"
+        $testResultMarkdown = "Well done. Your tenant has [SearchQueryInitiated audit log]($portalLink) enabled for all users.`n`n%TestResult%"
     } else {
-        $testResultMarkdown = "Your tenant does not have [SearchQueryInitiated audit log enabled]($portalLink).`n`n%TestResult%"
+        $testResultMarkdown = "Your tenant does not have [SearchQueryInitiated audit log]($portalLink) enabled for all users.`n`n%TestResult%"
     }
 
-    $result = "| Mailbox | SearchQueryInitiated |`n"
+    $result = "$passResult ($(($mailboxes.count - $resultMailboxes.count)) of $($mailboxes.count)) $failResult ($($resultMailboxes.count) of $($mailboxes.count)). Showing first 100.`n`n"
+    $result += "| Mailbox | SearchQueryInitiated |`n"
     $result += "| --- | --- |`n"
     foreach($item in $mailboxes | Sort-Object -Property UserPrincipalName){
         if($item.Guid -notin $resultMailboxes.Guid){
