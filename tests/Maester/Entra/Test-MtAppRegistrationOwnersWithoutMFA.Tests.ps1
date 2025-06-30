20c8a251f0f229693b537cff7bcfda359bede7f6
marcel.nguyen <marcel.nguyen@outlook.de>
2025-06-30 18:47:20 +0200

    fixing test file

--- tests/Maester/Entra/Test-MtAppRegistrationOwnersWithoutMFA.Tests.ps1
+++ tests/Maester/Entra/Test-MtAppRegistrationOwnersWithoutMFA.Tests.ps1
@@ -1,7 +1,3 @@
-BeforeAll {
-    . $PSScriptRoot/Test-MtAppRegistrationOwnersWithoutMFA.ps1
-}
-
 Describe "Maester/Entra" -Tag "MT.1063", "Entra", "Security", "Applications" {
     It "MT.1063: All Application Owners should have MFA set up" {
         $result = Test-MtAppRegistrationOwnerWithoutMFA
