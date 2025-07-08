4316424ec38eccc44efb916fba26cb7530cf18de
Robbe Van den Daele <107618465+RobbeVandenDaele@users.noreply.github.com>
2025-07-08 15:46:00 +0200

    Add test: MT.1061 Mfa on device registration conflict (#1001)
    Co-authored-by: Merill Fernando <1288081+merill@users.noreply.github.com>

--- tests/Maester/Entra/Test-ConditionalAccessBaseline.Tests.ps1
+++ tests/Maester/Entra/Test-ConditionalAccessBaseline.Tests.ps1
@@ -71,6 +71,9 @@
     It "MT.1052: At least one Conditional Access policy is targeting the Device Code authentication flow. See https://maester.dev/docs/tests/MT.1052" -Tag "MT.1052" {
         Test-MtCaDeviceCodeFlow | Should -Be $true -Because "there is no policy that targets the device code authentication flow."
     }
+    It "MT.1061: Device registration MFA control conflicts with Conditional Access policies. See https://maester.dev/docs/tests/MT.1061" -Tag "MT.1061" {
+        Test-MtDeviceRegistrationMfaConflict | Should -Be $true -Because "there is a conflict between Entra ID settings and Conditional Access policies regarding MFA during device registration."
+    }
     Context "Maester/Entra" -Tag "Entra", "License" {
         It "MT.1022: All users utilizing a P1 license should be licensed. See https://maester.dev/docs/tests/MT.1022" -Tag "MT.1022" {
             $LicenseReport = Test-MtCaLicenseUtilization -License "P1"
