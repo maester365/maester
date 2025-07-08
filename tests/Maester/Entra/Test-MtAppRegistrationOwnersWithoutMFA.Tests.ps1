425402f3c518d3b0055efdda6d3e55f005690caa
marcel.nguyen <marcel.nguyen@outlook.de>
2025-07-08 16:39:04 +0200

    fixed typo in function name

--- tests/Maester/Entra/Test-MtAppRegistrationOwnersWithoutMFA.Tests.ps1
+++ tests/Maester/Entra/Test-MtAppRegistrationOwnersWithoutMFA.Tests.ps1
@@ -1,6 +1,6 @@
 Describe "Maester/Entra" -Tag "MT.1063", "Entra", "Security", "Applications" {
     It "MT.1063: All App registration owners should have MFA registered" {
-        $result = Test-MtAppRegistrationOwnerWithoutMFA
+        $result = Test-MtAppRegistrationOwnersWithoutMFA
         $result | Should -Be $true -Because "All App registration owners should have MFA registered."
     }
 }
