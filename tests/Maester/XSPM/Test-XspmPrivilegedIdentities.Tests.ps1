fae0ae09dd6ed075bd03d2275eeaf5a6857283b6
Thomas Naunheim <47817884+Cloud-Architekt@users.noreply.github.com>
2025-08-21 13:23:44 +0200

    Updated description and display output for XSPM checks

--- tests/Maester/XSPM/Test-XspmPrivilegedIdentities.Tests.ps1
+++ tests/Maester/XSPM/Test-XspmPrivilegedIdentities.Tests.ps1
@@ -18,7 +18,11 @@ BeforeDiscovery {
 Describe "Exposure Management - Protection of classified assets identified by EntraOps and Critical Asset Management" -Tag "Full", "Maester", "Privileged", "XSPM", "EntraOps" -Skip:( $EntraIDPlan -ne "P2" ) {
     It "MT.1071: App registrations with privileged API permissions should have no owners. See https://maester.dev/docs/tests/MT.1071" -Tag "MT.1071" {
 
-    $UnifiedIdentityInfo = Get-MtXspmUnifiedIdentityInfo
+    try {
+        $UnifiedIdentityInfo = Get-MtXspmUnifiedIdentityInfo
+    } catch {
+        Write-Verbose "Authentication needed. Please call Connect-MgGraph."
+    }
 
     $HighPrivilegedAppsByApiPermissions = $UnifiedIdentityInfo | where-object {$_.ApiPermissions.Classification -eq "ControlPlane" -or $_.ApiPermissions.Classification -eq "ManagementPlane" -or $_.ApiPermissions.PrivilegeLevel -eq "High" }
     $SensitiveApiRolesOnAppsWithOwners = $HighPrivilegedAppsByApiPermissions | Where-Object { $null -ne $_.OwnedBy -and $_.Type -eq "Workload" }
@@ -50,8 +54,11 @@ Describe "Exposure Management - Protection of classified assets identified by En
                     $Owner = $($_.NodeName) - $($_.NodeLabel)
                     $TierBreach = "Unknown"
                 }
+
+                # Use <br/> for GitHub Flavored Markdown in-table line breaks
                 $AppRoleDisplayName = $filteredApiPermissions.AppRoleDisplayName -join "<br/>"
-                $AppDisplayName = $filteredApiPermissions.AppDisplayName -join "\"
+                $AppDisplayName = $filteredApiPermissions.AppDisplayName -join "<br/>"
+
                 $ServicePrincipalLink = "[$($SensitiveApp.AccountDisplayName)](https://entra.microsoft.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/$($SensitiveApp.AppId)/isMSAApp~/false)"
                 $result += "| $($AdminTierLevelIcon) $($ServicePrincipalLink) | $($Owner) | $($TierBreach) | $($AppRoleDisplayName) | $($AppDisplayName) |`n"
             }
@@ -75,6 +82,8 @@ Describe "Exposure Management - Protection of classified assets identified by En
             Write-Verbose "Authentication needed. Please call Connect-MgGraph."
         }
 
+        $Severity = "Medium"
+        $ShouldBeReason = "ownership should not be used for high-privileged apps"
         $HighPrivilegedAppsByEntraRoles = $UnifiedIdentityInfo | where-object {$_.AssignedEntraRoles.Classification -eq "ControlPlane" -or $_.AssignedEntraRoles.Classification -eq "ManagementPlane" -or $_.AssignedEntraRoles.RoleIsPrivileged -eq $True }
         $SensitiveDirectoryRolesOnAppsWithOwners = $HighPrivilegedAppsByEntraRoles | Where-Object { $null -ne $_.OwnedBy -and $_.Type -eq "Workload" }
 
@@ -108,16 +117,26 @@ Describe "Exposure Management - Protection of classified assets identified by En
                         $Owner = $($_.NodeName) - $($_.NodeLabel)
                         $TierBreach = "Unknown"
                     }
-                    $filteredApiPermissions.RoleDefinitionName = $filteredApiPermissions.RoleDefinitionName -join '`'
-                    $filteredApiPermissions.RoleScope = $filteredApiPermissions.RoleScope -join '\\'
+
+                    # Increase severity to high if tier breach detected
+                    if ($TierBreach -eq $true) {
+                        $Severity = "High"
+                        $ShouldBeReason = "ownership should not be used for high-privileged apps and also not delegated to lower privileged users"
+                    }
+                    $filteredApiPermissions.RoleDefinitionName = "$($filteredApiPermissions.RoleDefinitionName)"
+                    # Use <br/> for GitHub Flavored Markdown in-table line breaks
+                    $RoleDefinitionName = $filteredApiPermissions.RoleDefinitionName -join "<br/>"
+                    $RoleScope = $filteredApiPermissions.RoleScope -join "<br/>"
+
                     $ServicePrincipalLink = "[$($SensitiveApp.AccountDisplayName)](https://entra.microsoft.com/#view/Microsoft_AAD_IAM/ManagedAppMenuBlade/~/Overview/objectId/$($SensitiveApp.AccountObjectId)/appId/$($SensitiveApp.AppId))"
-                    $result += "| $($AdminTierLevelIcon) $($ServicePrincipalLink) | $($Owner) | $($TierBreach) | $($filteredApiPermissions.RoleDefinitionName) | $($filteredApiPermissions.RoleScope) |`n"
+                    $result += "| $($AdminTierLevelIcon) $($ServicePrincipalLink) | $($Owner) | $($TierBreach) | $($RoleDefinitionName) | $($RoleScope) |`n"
                 }
             }
             $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $result
         }
-        Add-MtTestResultDetail -Result $testResultMarkdown -Description "Description Field" -Severity "High"
-        $SensitiveApiRolesOnAppsWithOwners.Count -eq "0" | Should -Be $True -Because $benefits
+
+        Add-MtTestResultDetail -Result $testResultMarkdown -Description "Description Field" -Severity $Severity
+        $SensitiveDirectoryRolesOnAppsWithOwners.Count -eq "0" | Should -Be $True -Because $ShouldBeReason
     }
 
     It "MT.1073: Privileged API permissions on workload identities should not be unused. See https://maester.dev/docs/tests/MT.1073" -Tag "MT.1073" {
@@ -133,6 +152,8 @@ Describe "Exposure Management - Protection of classified assets identified by En
             Write-Verbose "Authentication needed. Please call Connect-MgGraph."
         }
 
+        $Severity = "Medium"
+        $ShouldBeReason = "unused sensitive API permissions should not be present"
         $HighPrivilegedAppsByApiPermissions = $UnifiedIdentityInfo | where-object {$_.ApiPermissions.Classification -eq "ControlPlane" -or $_.ApiPermissions.Classification -eq "ManagementPlane" -or $_.ApiPermissions.PrivilegeLevel -eq "High" }
         $SensitiveAppsWithUnusedPermissions = $HighPrivilegedAppsByApiPermissions | Where-Object { $_.ApiPermissions.InUse -eq $false }
 
@@ -156,8 +177,8 @@ Describe "Exposure Management - Protection of classified assets identified by En
             }
             $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $result
         }
-        Add-MtTestResultDetail -Result $testResultMarkdown
-        $SensitiveApiRolesOnAppsWithOwners.Count -eq "0" | Should -Be $True -Because $benefits
+        Add-MtTestResultDetail -Result $testResultMarkdown -Severity $Severity -Description "High privileged app registration will be identified by using data from OAuthAppInfo and the high privilege level by MDA App Governance but also Control Plane and Management Plane classification by the community project EntraOps. Unused API permissions will be identified by Exposure Management."
+        $SensitiveAppsWithUnusedPermissions.Count -eq "0" | Should -Be $True -Because $ShouldBeReason
     }
 
     It "MT.1074: Credentials, token or cookies from high-privileged users should not be exposed on vulnerable endpoints. See https://maester.dev/docs/tests/MT.1074" -Tag "MT.1074" {
@@ -174,6 +195,9 @@ Describe "Exposure Management - Protection of classified assets identified by En
             Write-Verbose "Authentication needed. Please call Connect-MgGraph."
         }
 
+        $Severity = "Medium"
+        $ShouldBeReason = "no exposed authentication artifacts should be present on vulnerable endpoints"
+
         if ($return) {
             $testResultMarkdown = "Well done. No authentication artifacts seems to be exposed on vulnerable endpoints."
         } else {
@@ -186,18 +210,27 @@ Describe "Exposure Management - Protection of classified assets identified by En
                 $EnrichedUserDetails = $UnifiedIdentityInfo | Where-Object { $_.AccountObjectId -eq $ExposedTokenArtifact.AccountObjectId } | Select-Object Classification, AccountObjectId, CriticalityLevel, AccountDisplayName, TenantId
                 if ($EnrichedUserDetails.Classification -eq "ControlPlane" -or $EnrichedUserDetails.Classification -eq "ManagementPlane" -or $EnrichedUserDetails.CriticalityLevel -lt "1") {
                     $AdminTierLevelIcon = Get-MtXspmPrivilegedClassificationIcon -AdminTierLevelName $EnrichedUserDetails.Classification
+
+                    if($EnrichedUserDetails.Classification -eq "ControlPlane") {
+                        $Severity = "High"
+                        $ShouldBeReason = "no exposed authentication artifacts should be present on vulnerable endpoints, especially Control Plane users."
+                    }
+
                     $UserLink = "[$($EnrichedUserDetails.AccountDisplayName)](https://entra.microsoft.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/overview/userId/$($EnrichedUserDetails.AccountObjectId))"
                     $DeviceLink = "[$($ExposedTokenArtifact.Device)](https://security.microsoft.com/machines/v2/$($ExposedTokenArtifact.DeviceId)?tid=$($EnrichedUserDetails.TenantId))"
-                    $UserArtifacts = $ExposedTokenArtifact.TokenArtifacts | foreach-object { (Get-MtXspmAuthenticationArtifactIcon -ArtifactType $_) + " " + $_ }
-                    $UserArtifacts = $UserArtifacts -join "`n"
+                    $UserArtifacts = $ExposedTokenArtifact.TokenArtifacts | ForEach-Object { (Get-MtXspmAuthenticationArtifactIcon -ArtifactType $_) + " " + $_ } | Where-Object { $_ -and $_.Trim() -ne '' } | ForEach-Object { $_.Trim() }
+
+                    # Use <br/> for GitHub Flavored Markdown in-table line breaks
+                    $UserArtifacts = $UserArtifacts -join "<br/>"
+
                     $result += "| $($AdminTierLevelIcon) $($UserLink)  | $($DeviceLink) | $($EnrichedUserDetails.Classification) | $($EnrichedUserDetails.CriticalityLevel) | $($UserArtifacts) | $($ExposedTokenArtifact.ExposureScore) | $($ExposedTokenArtifact.RiskScore) |`n"
                 }
             }
             $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $result
         }
-        Add-MtTestResultDetail -Result $testResultMarkdown
+        Add-MtTestResultDetail -Result $testResultMarkdown -Severity $Severity -Description "High privileged users will be identified by using data from IdentityInfo and OAuthAppInfo. Exposed authentication artifacts will be identified by Exposure Management."
 
-        $ExposedTokenArtifacts.Count -eq "0" | Should -Be $True -Because $benefits
+        $ExposedTokenArtifacts.Count -eq "0" | Should -Be $True -Because $ShouldBeReason
     }
 
     It "MT.1075: Hybrid users should not be assigned to Entra ID role assignments. See https://maester.dev/docs/tests/MT.1075" -Tag "MT.1075" {
@@ -206,6 +239,9 @@ Describe "Exposure Management - Protection of classified assets identified by En
 
         $HighPrivilegedUsersByEntraRoles = $UnifiedIdentityInfo | Where-Object { $_.Type -eq "User" -and ($_.AssignedEntraRoles.Classification -eq "ControlPlane" -or $_.AssignedEntraRoles.Classification -eq "ManagementPlane" -or $_.AssignedEntraRoles.RoleIsPrivileged -eq $True) | Sort-Object Classification, AccountDisplayName}
         $HighPrivilegedHybridUsers = $HighPrivilegedUsersByEntraRoles | Where-Object { $_.SourceProviders -eq "AzureActiveDirectory"}
+
+        $Severity = "Medium"
+        $ShouldBeReason = "no hybrid users with sensitive directory roles should be present"
 
         if ($return) {
             $testResultMarkdown = "Well done. No hybrid users with sensitive directory roles."
@@ -219,16 +255,24 @@ Describe "Exposure Management - Protection of classified assets identified by En
                 $UserSensitiveDirectoryRoles = $filteredDirectoryRoles | foreach-object { (Get-MtXspmPrivilegedClassificationIcon -AdminTierLevelName $_.Classification) + " " + $_.RoleDefinitionName }
                 $UserSensitiveDirectoryRolesResult = @()
                 $UserSensitiveDirectoryRoles | ForEach-Object {
-                    $UserSensitiveDirectoryRolesResult += "* $_ `n"
+                    $UserSensitiveDirectoryRolesResult += "$_, "
                 }
                 $AdminTierLevelIcon = Get-MtXspmPrivilegedClassificationIcon -AdminTierLevelName $HighPrivilegedHybridUser.Classification
+                if ($HighPrivilegedHybridUser.Classification -eq "ControlPlane") {
+                    $Severity = "High"
+                    $ShouldBeReason = "no hybrid users with sensitive directory roles should be present, especially Control Plane users."
+                }
+
+                # Use <br/> for GitHub Flavored Markdown in-table line breaks
+                $UserSensitiveDirectoryRolesResult = $UserSensitiveDirectoryRolesResult -join "<br/>"
+
                 $HybridUserLink = "[$($HighPrivilegedHybridUser.AccountDisplayName)](https://entra.microsoft.com/#view/Microsoft_AAD_UsersAndTenants/UserProfileMenuBlade/~/overview/userId/$($HighPrivilegedHybridUser.AccountObjectId))"
                 $result += "| $($AdminTierLevelIcon) $($HybridUserLink) | $($HighPrivilegedHybridUser.Classification) | $($UserSensitiveDirectoryRolesResult) | $($HighPrivilegedHybridUser.SourceProvider) |`n"
             }
             $testResultMarkdown = $testResultMarkdown -replace "%TestResult%", $result
         }
-        Add-MtTestResultDetail -Result $testResultMarkdown
+        Add-MtTestResultDetail -Result $testResultMarkdown -Severity $Severity -Description "High privileged users will be identified by using data from IdentityInfo. Sensitive directory roles will be identified by using classification from community project EntraOps."
 
-        $HighPrivilegedHybridUsers.Count -eq "0" | Should -Be $True -Because $benefits
+        $HighPrivilegedHybridUsers.Count -eq "0" | Should -Be $True -Because $ShouldBeReason
     }
 }
