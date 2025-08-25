deae0ee971d14913a09883942af611363f279be1
Sam Erde <samuel.erde@gmail.com>
2025-08-25 11:33:57 -0400

    Remove 'All' tag from CIS tests.

--- tests/cis/Test-MtCisTeamsLobbyBypass.Tests.ps1
+++ tests/cis/Test-MtCisTeamsLobbyBypass.Tests.ps1
@@ -1,4 +1,4 @@
-Describe "CIS" -Tag "All", "Security", "CIS", "CIS M365 v5.0.0" {
+Describe "CIS" -Tag "Security", "CIS", "CIS M365 v5.0.0" {
     It "CIS.M365.8.5.3: (L1) Ensure only people in my org can bypass the lobby" -Tag "CIS.M365.8.5.3", "CIS E3 Level 1" {
 
         $result = Test-MtCisTeamsLobbyBypass
