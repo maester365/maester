8bb1c4c9b6ef19bd6fcd1ccc34737f7620f74240
Henrik Piecha <henrik.piecha@bridging-it.de>
2025-08-25 08:56:24 +0200

    added try-catch code block and changed description to its origin

--- powershell/public/maester/exchange/Test-MtLimitOnMicrosoftDomainUsage.ps1
+++ powershell/public/maester/exchange/Test-MtLimitOnMicrosoftDomainUsage.ps1
@@ -5,7 +5,7 @@
 .DESCRIPTION
     This test checks if any mailbox is using the .onmicrosoft.com domain as primary SMTP address.
     Usage of the .onmicrosoft.com domain has its limitation and receives throttling.
-    
+
 .EXAMPLE
     Test-MtLimitOnMicrosoftDomainUsage
 
@@ -18,26 +18,30 @@ function Test-MtLimitOnMicrosoftDomainUsage {
     [CmdletBinding()]
     [OutputType([bool])]
     param()
-    
+
     if (-not (Test-MtConnection ExchangeOnline)) {
         Add-MtTestResultDetail -SkippedBecause NotConnectedExchange
         return $null
-    } 
+    }
     $return = $true
     Write-Verbose "Checking if mailboxes use the .onmicrosoft.com domain as primary SMTP address..."
-
-    $allMbx = Get-Mailbox | Where-Object { $_.PrimarySmtpAddress -like "*@*.onmicrosoft.com" }
-    if (($allMbx | Measure-Object).Count -eq 0) {
-        $result = "Well done. No mailbox uses the .onmicrosoft.com domain as primary SMTP address."
-        Add-MtTestResultDetail -Result $result
-    } else {
-        $mgUsers = @()
-        foreach ($mbx in $allMbx) {
-            $mgUsers += Invoke-MtGraphRequest -RelativeUri "users" -UniqueId $mbx.ExternalDirectoryObjectId
+    try {
+        $allMbx = Get-Mailbox | Where-Object { $_.PrimarySmtpAddress -like "*@*.onmicrosoft.com" }
+        if (($allMbx | Measure-Object).Count -eq 0) {
+            $result = "Well done. No mailbox uses the .onmicrosoft.com domain as primary SMTP address."
+            Add-MtTestResultDetail -Result $result
+        } else {
+            $mgUsers = @()
+            foreach ($mbx in $allMbx) {
+                $mgUsers += Invoke-MtGraphRequest -RelativeUri "users" -UniqueId $mbx.ExternalDirectoryObjectId
+            }
+            $result = "Your tenant has $(($allMbx | Measure-Object).Count) mailboxes using the .onmicrosoft.com domain as primary SMTP address:`n`n%TestResult%"
+            $return = $false
+            Add-MtTestResultDetail -Result $result -GraphObjects $mgUsers -GraphObjectType Users
         }
-        $result = "Your tenant has $(($allMbx | Measure-Object).Count) mailboxes using the .onmicrosoft.com domain as primary SMTP address:`n`n%TestResult%"
-        $return = $false
-        Add-MtTestResultDetail -Result $result -GraphObjects $mgUsers -GraphObjectType Users
-    } 
-    return $return
+        return $return
+    } catch {
+        Add-MtTestResultDetail -SkippedBecause Error -SkippedError $_
+        return $null
+    }
 }
