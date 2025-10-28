6b1acde240eee379cec1e45427c72ab16c9e30d5
Thomas S. Schmidt <thosc@tdc.dk>
2025-10-28 11:56:03 +0100

    ConvertFrom-MailAuthenticationRecordDkim return if no record is found

--- powershell/public/cisa/exchange/ConvertFrom-MailAuthenticationRecordDkim.ps1
+++ powershell/public/cisa/exchange/ConvertFrom-MailAuthenticationRecordDkim.ps1
@@ -105,6 +105,9 @@ function ConvertFrom-MailAuthenticationRecordDkim {
                 $dkimRecord = [DKIMRecord]::new((Resolve-DnsName @dkimSplat | `
                             Where-Object { $_.Type -eq "TXT" } | `
                             Where-Object { $_.Strings -match $matchRecord }).Strings)
+                If (-not $dkimRecord) {
+                    return "Failure to obtain record"
+                }
             } else {
                 $cmdletCheck = Get-Command "Resolve-Dns" -ErrorAction SilentlyContinue
                 if ($cmdletCheck) {
