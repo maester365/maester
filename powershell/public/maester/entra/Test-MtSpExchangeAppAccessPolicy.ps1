4bf9df7e4a9d4b624914716cc3b34f1aa35ca4a4
Thomas S. Schmidt <thosc@tdc.dk>
2025-10-28 21:46:36 +0100

    Change warning to verbose instead

--- powershell/public/maester/entra/Test-MtSpExchangeAppAccessPolicy.ps1
+++ powershell/public/maester/entra/Test-MtSpExchangeAppAccessPolicy.ps1
@@ -63,7 +63,7 @@ function Test-MtSpExchangeAppAccessPolicy {
             $appAccessPolicies = Get-ApplicationAccessPolicy -ErrorAction Stop
         } catch {
             if ($_.Exception.Message -like "*couldn't be found*") {
-                Write-Warning -Message 'Test-MtSpExchangeAppAccessPolicy: No application access policies was found.'
+                Write-Verbose -Message 'Test-MtSpExchangeAppAccessPolicy: No application access policies was found.'
                 $appAccessPolicies = $null
             } else {
                 throw
