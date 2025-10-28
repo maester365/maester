0ed6c0826294f1de47ec05b3e7d63ceb7f7e6547
Thomas S. Schmidt <thosc@tdc.dk>
2025-10-28 22:22:09 +0100

    CIS.M365.1.3.6 add missing space in test name

--- tests/cis/Test-MtCisCustomerLockBox.Tests.ps1
+++ tests/cis/Test-MtCisCustomerLockBox.Tests.ps1
@@ -1,5 +1,5 @@
 Describe "CIS" -Tag "CIS.M365.1.3.6", "L2", "CIS E5 Level 2", "CIS E5", "CIS", "Security", "CIS M365 v5.0.0" {
-    It "CIS.M365.1.3.6:(L2) Ensure the customer lockbox feature is enabled" {
+    It "CIS.M365.1.3.6: (L2) Ensure the customer lockbox feature is enabled" {
 
         $result = Test-MtCisCustomerLockBox
 
