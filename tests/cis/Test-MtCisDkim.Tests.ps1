a9c586407ddd39d785214b7410651962fd5dcce9
Thomas S. Schmidt <thosc@tdc.dk>
2025-10-28 22:27:26 +0100

    Remove level number from test name

--- tests/cis/Test-MtCisDkim.Tests.ps1
+++ tests/cis/Test-MtCisDkim.Tests.ps1
@@ -1,5 +1,5 @@
 Describe "CIS" -Tag "CIS.M365.2.1.9", "L1", "CIS E3 Level 1", "CIS E3", "CIS", "Security", "CIS M365 v5.0.0" {
-    It "CIS.M365.2.1.9: (L1) Ensure that DKIM is enabled for all Exchange Online Domains" {
+    It "CIS.M365.2.1.9: Ensure that DKIM is enabled for all Exchange Online Domains" {
 
         $result = Test-MtCisDkim
 
