5ce8edffce7d911f14b49ab08e0b7d383c970724
Thomas S. Schmidt <thosc@tdc.dk>
2025-10-29 14:51:58 +0100

    MT.1024 refactor markdown generation to align with other tests and support new Defender for Identity recommendations

--- tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
+++ tests/Maester/Entra/Test-EntraRecommendations.Tests.ps1
@@ -15,37 +15,13 @@ Describe "Maester/Entra" -Tag "Maester", "Entra", "Security", "Recommendation" -
             "userRiskPolicy",
             "signinRiskPolicy"
         )
+        #region Build test result markdown
         $recommendationUrl = "https://entra.microsoft.com/#view/Microsoft_AAD_IAM/RecommendationDetails.ReactView/recommendationId/$($_.id)"
         $recommendationLinkMd = "`n`n➡️ Open [Recommendation - $($_.displayName)]($recommendationUrl) in the Entra admin portal.`n`n*Note: If the recommendation is not applicable for your tenant, it can be marked as **Dismissed** for Maester to skip it in the future.*"
-        $actionSteps = $actionSteps | Sort-Object -Property 'stepNumber' | ForEach-Object {
-            if ($_.actionUrl.url) {
-                $actionLink = " [$($_.actionUrl.displayName)]($($_.actionUrl.url.replace('" \l "','#')))."
-            }
-            $_.text.replace("<br>","`n`n") + $actionLink
-        }
-        $actionSteps = $ActionSteps -join "`n`n"
-        $recommendationDescription = "$($_.benefits)`n`n#### Remediation action:`n`n$($_.remediationImpact)`n`n${actionSteps}"
-
-        $EntraIDPlan = Get-MtLicenseInformation -Product "EntraID"
-        if ( $EntraIDPlan -ne "P2" ) {
-            $EntraPremiumRecommendations | ForEach-Object {
-                if ( $id -match "$($_)$" ) {
-                    Add-MtTestResultDetail -Description $recommendationDescription -Severity $_.priority -SkippedBecause NotLicensedEntraIDP2
-                    return $null
-                }
-            }
-        }
-
-        if ( $status -match "dismissed" ) {
-            Add-MtTestResultDetail -Description $benefits -Severity $_.priority -SkippedBecause Custom -SkippedCustomReason "This recommendation has been **Dismissed** by an administrator.`n`nIf this test is valid for your tenant you can change its state from **Dismissed** to **Active**. $recommendationLinkMd"
-            return $null
-        }
-
-        #region Add detailed test description
-        if ($status -ne 'completedBySystem' -and $impactedResources) {
+        if ($status -ne 'completedBySystem' -and $_.impactedResources) {
             $impactedResourcesList = "`n`n#### Impacted resources`n`n | Status | Name | First detected| `n"
             $impactedResourcesList += "| --- | --- | --- |`n"
-            foreach ($resource in $impactedResources) {
+            foreach ($resource in $_.impactedResources) {
                 if ($resource.status -eq 'completedBySystem') {
                     $resourceResult = "✅ Pass"
                 } else {
@@ -54,10 +30,35 @@ Describe "Maester/Entra" -Tag "Maester", "Entra", "Security", "Recommendation" -
                 $impactedResourcesList += "| $($resourceResult) | [$($resource.displayName)]($($resource.portalUrl)) | $($resource.addedDateTime) | `n"
             }
         }
-
-        $ResultMarkdown = $insights + $impactedResourcesList + $recommendationLinkMd
-        Add-MtTestResultDetail -Description $recommendationDescription -Result $ResultMarkdown -Severity $_.priority
+        $resultMd = $_.insights + $impactedResourcesList + $recommendationLinkMd
         #endregion
+        #region Build test description markdown
+        $actionSteps = $_.actionSteps | Sort-Object -Property 'stepNumber' | ForEach-Object {
+            if ($_.actionUrl.url) {
+                $actionLink = " [$($_.actionUrl.displayName)]($($_.actionUrl.url.replace('" \l "','#')))."
+            }
+            ($_.text.replace("<br>","`n`n").replace("<br/>","`n`n") -replace "<a.+?href=[`"']([^`"']+)[`"'].+?>([^<]+)<\/a>", '[$2]($1)') + $actionLink
+        }
+        $actionSteps = $actionSteps -join "`n`n"
+        $descriptionMd = "$($_.benefits)`n`n#### Remediation action:`n`n${actionSteps}`n`n**Impact:** $($_.remediationImpact)"
+        #endregion
+
+        $EntraIDPlan = Get-MtLicenseInformation -Product "EntraID"
+        if ( $EntraIDPlan -ne "P2" ) {
+            $EntraPremiumRecommendations | ForEach-Object {
+                if ( $id -match "$($_)$" ) {
+                    Add-MtTestResultDetail -Description $descriptionMd -Severity $_.priority -SkippedBecause NotLicensedEntraIDP2
+                    return $null
+                }
+            }
+        }
+
+        if ( $status -match "dismissed" ) {
+            Add-MtTestResultDetail -Description $descriptionMd -Severity $_.priority -SkippedBecause Custom -SkippedCustomReason "This recommendation has been **Dismissed** by an administrator.`n`nIf this test is valid for your tenant you can change its state from **Dismissed** to **Active**. $recommendationLinkMd"
+            return $null
+        }
+        Add-MtTestResultDetail -Description $descriptionMd -Severity $_.priority -Result $resultMd
+
         # Actual test
         $status | Should -Be "completedBySystem" -Because $benefits
     }
