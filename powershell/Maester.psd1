8e323055123d6803f5cc1a910e773839cc8f783a
RobbeVandenDaele <vandendaele.robbe@outlook.com>
2025-11-06 22:25:58 +0100

    Add CriticalCredentialsOnNonTpmProtectedDevices test

--- powershell/Maester.psd1
+++ powershell/Maester.psd1
@@ -171,7 +171,8 @@ FunctionsToExport = 'Add-MtTestResultDetail',
     'Test-MtXspmHybridUsersWithAssignedEntraIdRoles',
     'Test-MtXspmPendingApprovalCriticalAssetManagement',
     'Test-MtXspmCriticalCredsOnDevicesWithNonCriticalAccounts',
-    'Test-MtXspmPublicRemotelyExploitableHighExposureDevices'
+    'Test-MtXspmPublicRemotelyExploitableHighExposureDevices',
+    'Test-MtXspmCriticalCredentialsOnNonTpmProtectedDevices'
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = @()
--- tests/XSPM/Test-XspmCriticalAssetManagement.Tests.ps1
+++ tests/XSPM/Test-XspmCriticalAssetManagement.Tests.ps1
@@ -13,12 +13,17 @@ Describe "Exposure Management" -Tag "Entra", "Graph", "Security", "XSPM" -Skip:(
     }
 
     # Devices should not share both critical and non-critical user credentials.
-    It "MT.XXX1: Devices should not share both critical and non-critical user credentials. See https://maester.dev/docs/tests/MT.XXX1" -Tag "MT.XXX1" {
+    It "MT.1086: Devices should not share both critical and non-critical user credentials. See https://maester.dev/docs/tests/MT.1086" -Tag "MT.1086" {
         Test-MtXspmCriticalCredsOnDevicesWithNonCriticalAccounts | Should -Be $true -Because "Devices should not share both critical and non-critical user credentials, as this may lead to potential security risks and compromise of critical assets via a non-critical account."
     }
 
     # Devices should not be public exposed with remotely exploitable, highly likely to be exploited, high or critical severity CVE's.
-    It "MT.XXX2: Devices should not be public exposed with remotely exploitable, highly likely to be exploited, high or critical severity CVE's. See https://maester.dev/docs/tests/MT.XXX2" -Tag "MT.XXX2" {
+    It "MT.1087: Devices should not be public exposed with remotely exploitable, highly likely to be exploited, high or critical severity CVE's. See https://maester.dev/docs/tests/MT.1087" -Tag "MT.1087" {
         Test-MtXspmPublicRemotelyExploitableHighExposureDevices | Should -Be $true -Because "Devices should not be public exposed with remotely exploitable, highly likely to be exploited, high or critical severity CVE's. Such devices are at high risk of being compromised by attackers, potentially leading to data breaches and other security incidents."
+    }
+
+    # Devices with critical credentials should be protected by TPM.
+    It "MT.1088: Devices with critical credentials should be protected by TPM. See https://maester.dev/docs/tests/MT.1088" -Tag "MT.1088" {
+        Test-MtXspmCriticalCredentialsOnNonTpmProtectedDevices | Should -Be $true -Because "Devices with critical credentials should be protected by TPM. Without TPM protection, these devices are vulnerable to various attacks that could compromise sensitive credentials, leading to potential security breaches."
     }
 }
