d5ff6750c8434ee6321e61216a27ffac5346c088
Benjamin Metz <benjamin.metz@oediv.de>
2026-02-06 08:38:36 +0100

    refactor: Change markdown result output

--- powershell/public/cis/Test-MtCisThirdPartyStorageServicesRestricted.ps1
+++ powershell/public/cis/Test-MtCisThirdPartyStorageServicesRestricted.ps1
@@ -24,9 +24,9 @@
         }
 
         if ($testResult) {
-            $testResultMarkdown = "Well done. Your tenants settings matches CIS recommendations.`n`n%TestResult%"
+            $testResultMarkdown = "Well done. Your tenant settings comply with CIS recommendations.`n`n%TestResult%"
         } else {
-            $testResultMarkdown = "Your tenants settings not matches CIS recommendations`n`n%TestResult%"
+            $testResultMarkdown = "Your tenant settings not comply with CIS recommendations.`n`n%TestResult%"
         }
 
         $resultMd = "| Setting | Result |`n"
