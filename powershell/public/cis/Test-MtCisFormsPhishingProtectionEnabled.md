d262f107e1dc8036113ac452753e58e2dc5d2bb9
Benjamin Metz <benjamin.metz@oediv.de>
2026-02-06 08:49:55 +0100

    feat: add CIS 1.3.5 test

---
+++ powershell/public/cis/Test-MtCisFormsPhishingProtectionEnabled.md
@@ -0,0 +1,21 @@
+1.3.4 (L1) Ensure 'User owned apps and services' is restricted
+
+**Rationale:**
+Enabling internal phishing protection for Microsoft Forms will prevent attackers using forms for phishing attacks by asking personal or other sensitive information and URLs.
+
+#### Remediation action:
+
+To enable notifications for internal users sending malware:
+1. Navigate to Microsoft 365 admin center [https://admin.microsoft.com](https://admin.microsoft.com).
+2. Click to expand **Settings** select **Org settings**.
+3. In **Services** select **Microsoft Forms**
+4. Check **Add internal phishing protection** under **Phishing protection**
+5. Click Save.
+
+#### Related links
+
+* [Microsoft 365 Admin Center](https://admin.microsoft.com)
+* [CIS Microsoft 365 Foundations Benchmark v5.0.0 - Page 57](https://www.cisecurity.org/benchmark/microsoft_365)
+
+<!--- Results --->
+%TestResult%
---
+++ powershell/public/cis/Test-MtCisFormsPhishingProtectionEnabled.ps1
@@ -0,0 +1,52 @@
+﻿function Test-MtCisFormsPhishingProtectionEnabled {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param()
+
+    if (!(Test-MtConnection Graph)) {
+        Add-MtTestResultDetail -SkippedBecause NotConnectedGraph
+        return $null
+    }
+
+    $scopes = (Get-MgContext).Scopes
+    $permissionMissing = "OrgSettings-Forms.Read.All" -notin $scopes
+    if($permissionMissing){
+        Add-MtTestResultDetail -SkippedBecause Custom -SkippedCustomReason "Missing Scope OrgSettings-Forms.Read.All"
+        return $null
+    }
+
+    try {
+        Write-Verbose 'Getting settings...'
+        $settings = Invoke-MtGraphRequest -ApiVersion beta -RelativeUri "admin/forms/settings" -DisableCache
+
+        Write-Verbose 'Executing checks'
+        $CheckIsInOrgFormsPhishingScanEnabled = $settings | Where-Object { $_.isInOrgFormsPhishingScanEnabled -eq $true }
+
+        $testResult = (($CheckIsInOrgFormsPhishingScanEnabled | Measure-Object).Count -ge 1)
+
+        if ($testResult) {
+            $testResultMarkdown = "Well done. Your tenant settings comply with CIS recommendations.`n`n%TestResult%"
+        } else {
+            $testResultMarkdown = "Your tenant settings not comply with CIS recommendations.`n`n%TestResult%"
+        }
+
+        $resultMd = "| Setting | Result |`n"
+        $resultMd += "| --- | --- |`n"
+
+        if ($CheckIsInOrgFormsPhishingScanEnabled) {
+            $CheckIsInOrgFormsPhishingScanEnabledResult = '✅ Pass'
+        } else {
+            $CheckIsInOrgFormsPhishingScanEnabledResult = '❌ Fail'
+        }
+
+        $resultMd += "| Add internal phishing protection | $CheckIsInOrgFormsPhishingScanEnabledResult |`n"
+
+        $testResultMarkdown = $testResultMarkdown -replace '%TestResult%', $resultMd
+
+        Add-MtTestResultDetail -Result $testResultMarkdown
+        return $testResult
+    } catch {
+        Add-MtTestResultDetail -SkippedBecause Error -SkippedError $_
+        return $null
+    }
+}
