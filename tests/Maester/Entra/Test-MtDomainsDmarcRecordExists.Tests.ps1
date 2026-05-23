92bdb0e504a204efffd7a9de37223090aeda1c39
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2026-05-23 12:07:50 +0200

    variable naming consistency on Exists

--- tests/Maester/Entra/Test-MtDomainsDmarcRecordExists.Tests.ps1
+++ tests/Maester/Entra/Test-MtDomainsDmarcRecordExists.Tests.ps1
@@ -1,9 +1,9 @@
 Describe "Maester/Entra" -Tag "Maester","Entra", "MT.1157" {
     It "MT.1157: A DMARC policy SHALL be published for every Entra managed and verified domain." {
-        $mtDomainsDmarcRecordExist = Test-MtDomainsDmarcRecordExists
+        $mtDomainsDmarcRecordExists = Test-MtDomainsDmarcRecordExists
 
-        if ($null -ne $mtDomainsDmarcRecordExist) {
-            $mtDomainsDmarcRecordExist | Should -Be $true -Because "DMARC record should exist."
+        if ($null -ne $mtDomainsDmarcRecordExists) {
+            $mtDomainsDmarcRecordExists | Should -Be $true -Because "DMARC record should exist."
         }
     }
 }
