5e74b1b4c3a6b999c1951b15baee5a254fba776d
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2026-05-23 12:10:34 +0200

    fixing missing tag space

--- tests/Maester/Entra/Test-MtDomainsDmarcRecordExists.Tests.ps1
+++ tests/Maester/Entra/Test-MtDomainsDmarcRecordExists.Tests.ps1
@@ -1,4 +1,4 @@
-Describe "Maester/Entra" -Tag "Maester","Entra", "MT.1157" {
+Describe "Maester/Entra" -Tag "Maester", "Entra", "MT.1157" {
     It "MT.1157: A DMARC policy SHALL be published for every Entra managed and verified domain." {
         $mtDomainsDmarcRecordExists = Test-MtDomainsDmarcRecordExists
 
