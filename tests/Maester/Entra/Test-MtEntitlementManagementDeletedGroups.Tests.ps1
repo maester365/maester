2f2767fac841b596cacfde9c44db32ac0b97cce7
Jean-Philippe George <36743032+JeanPhilippeGeorge@users.noreply.github.com>
2026-05-29 10:35:28 +0200

    Refactoring

--- tests/Maester/Entra/Test-MtEntitlementManagementDeletedGroups.Tests.ps1
+++ tests/Maester/Entra/Test-MtEntitlementManagementDeletedGroups.Tests.ps1
@@ -1,7 +1,7 @@
 Describe "Maester/Entra" -Tag "Maester", "Governance", "Entra", "AccessPackages" {
     It "MT.1107: Access packages and catalogs should not reference deleted groups. See https://maester.dev/docs/tests/MT.1107" -Tag "MT.1107" {
         $result = Test-MtEntitlementManagementDeletedGroups
-        if ($result -ne $null) {
+        if ($null -ne $result) {
             $result | Should -Be $true -Because "Access packages and catalogs should not reference deleted groups to prevent access provisioning failures and configuration inconsistencies."
         }
     }
