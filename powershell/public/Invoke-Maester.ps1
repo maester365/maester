1e560a4e3728364a7c430d469498e4b6397e094d
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2026-06-18 20:09:51 +0200

    Adding markdown summary

--- powershell/public/Invoke-Maester.ps1
+++ powershell/public/Invoke-Maester.ps1
@@ -127,11 +127,14 @@
         # The path to the file to save the test results in markdown format. The filename should include a .md extension.
         [string] $OutputMarkdownFile,
 
+        # The path to the file to save a compact markdown summary with only result counters. The filename should include a .md extension.
+        [string] $OutputMarkdownSummaryFile,
+
         # The path to the file to save the test results in json format. The filename should include a .json extension.
         [string] $OutputJsonFile,
 
         # The folder to save the test results in. If no -Output* is set, defaults to ./test-results.
-        # If set, other -Output* parameters are ignored and all formats will be generated (markdown, html, json) with a timestamp and saved in the folder.
+        # If set, other -Output* parameters are ignored and all formats will be generated (markdown, markdown summary, html, json) with a timestamp and saved in the folder.
         [string] $OutputFolder,
 
         # The filename prefix to use for all the files in the output folder. e.g. 'TestResults' will generate TestResults.html, TestResults.md, TestResults.json.
@@ -226,6 +229,11 @@
                 $result = 'The OutputMarkdownFile parameter must have an .md extension.'
             }
         }
+        if (![string]::IsNullOrEmpty($out.OutputMarkdownSummaryFile)) {
+            if ($out.OutputMarkdownSummaryFile.EndsWith('.md') -eq $false) {
+                $result = 'The OutputMarkdownSummaryFile parameter must have an .md extension.'
+            }
+        }
         if (![string]::IsNullOrEmpty($out.OutputJsonFile)) {
             if ($out.OutputJsonFile.EndsWith('.json') -eq $false) {
                 $result = 'The OutputJsonFile parameter must have a .json extension.'
@@ -233,7 +241,8 @@
         }
 
         $someOutputFileHasValue = ![string]::IsNullOrEmpty($out.OutputHtmlFile) -or `
-            ![string]::IsNullOrEmpty($out.OutputMarkdownFile) -or ![string]::IsNullOrEmpty($out.OutputJsonFile)
+            ![string]::IsNullOrEmpty($out.OutputMarkdownFile) -or ![string]::IsNullOrEmpty($out.OutputJsonFile) -or `
+            ![string]::IsNullOrEmpty($out.OutputMarkdownSummaryFile)
 
         if ([string]::IsNullOrEmpty($out.OutputFolder) -and !$someOutputFileHasValue) {
             # No outputs specified. Set default folder.
@@ -252,6 +261,7 @@
 
             $out.OutputHtmlFile = Join-Path $out.OutputFolder "$($out.OutputFolderFileName).html"
             $out.OutputMarkdownFile = Join-Path $out.OutputFolder "$($out.OutputFolderFileName).md"
+            $out.OutputMarkdownSummaryFile = Join-Path $out.OutputFolder "$($out.OutputFolderFileName)-summary.md"
             $out.OutputJsonFile = Join-Path $out.OutputFolder "$($out.OutputFolderFileName).json"
 
             if ($ExportCsv.IsPresent) {
@@ -341,6 +351,7 @@
         OutputFolderFileName = $OutputFolderFileName
         OutputHtmlFile       = $OutputHtmlFile
         OutputMarkdownFile   = $OutputMarkdownFile
+        OutputMarkdownSummaryFile = $OutputMarkdownSummaryFile
         OutputJsonFile       = $OutputJsonFile
         OutputCsvFile        = $null
         OutputExcelFile      = $null
@@ -484,6 +495,12 @@
             Write-MtProgress -Activity 'Creating markdown report'
             $output = Get-MtMarkdownReport -MaesterResults $maesterResults
             $output | Out-File -FilePath $out.OutputMarkdownFile -Encoding UTF8
+        }
+
+        if (![string]::IsNullOrEmpty($out.OutputMarkdownSummaryFile)) {
+            Write-MtProgress -Activity 'Creating markdown summary report'
+            $output = Get-MtMarkdownSummaryReport -MaesterResults $maesterResults
+            $output | Out-File -FilePath $out.OutputMarkdownSummaryFile -Encoding UTF8
         }
 
         if (![string]::IsNullOrEmpty($out.OutputCsvFile)) {
